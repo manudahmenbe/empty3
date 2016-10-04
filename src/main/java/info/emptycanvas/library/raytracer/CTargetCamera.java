6c9fb0248f455d925813f3ea4c5d1333b5106762
Manuel Dahmen <manuel.dahmen@gmail.com>
2016-10-04 08:05:09 +0200

    RayTrace ALL PURPLE IMAGE NOW. Trying, again and again Formula in CSphere and CPlane for computing intersects needs checks

--- src/main/java/info/emptycanvas/library/raytracer/CTargetCamera.java
+++ src/main/java/info/emptycanvas/library/raytracer/CTargetCamera.java
@@ -3,10 +3,11 @@ package info.emptycanvas.library.raytracer;
 import info.emptycanvas.library.object.Point3D;
 
 public class CTargetCamera extends CCamera {
-    protected Point3D mLookAtPoint;    // Le point observé
+    /*protected Point3D mLookAtPoint;    // Le point observé
     private Point3D mVecDir = null;
     private Point3D mRightVec = null;
     private Point3D mViewPlaneUpLeft = null;
+    */
     public CTargetCamera(Point3D camPos, Point3D lookAtPoint, Point3D upVector) {
         super(camPos, lookAtPoint, lookAtPoint.prodVect(upVector).norme1(), upVector, TARGETCAMERA);
     }
