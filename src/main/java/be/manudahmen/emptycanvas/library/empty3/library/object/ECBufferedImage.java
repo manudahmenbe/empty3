b3203cd7e95efbcceb55aa22ef4b21c58bcfc4e6
Manuel Dahmen <manuel.dahmen@gmail.com>
2016-12-04 02:48:29 +0100

    Start copyright notices

--- src/main/java/be/manudahmen/emptycanvas/library/empty3/library/object/ECBufferedImage.java
+++ src/main/java/be/manudahmen/emptycanvas/library/empty3/library/object/ECBufferedImage.java
@@ -1,3 +1,15 @@
+/*
+ * Copyright (c) 2016. Tous les fichiers dans ce programme sont soumis à la License Publique Générale GNU créée par la Free Softxware Association, Boston.
+ * La plupart des licenses de parties tièrces sont compatibles avec la license principale.
+ * Les parties tierces peuvent être soumises à d'autres licenses.
+ * Montemedia : Creative Commons
+ * ECT : Tests à valeur artistique ou technique.
+ * La partie RayTacer a été honteusement copiée sur le Net. Puis traduite en Java et améliorée.
+ * Java est une marque de la société Oracle.
+ *
+ * Pour le moment le programme est entièrement accessible sans frais supplémentaire. Get the sources, build it, use it, like it, share it.
+ */
+
 package be.manudahmen.emptycanvas.library.empty3.library.object;
 
 import javax.imageio.ImageIO;
@@ -9,11 +21,12 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ECBufferedImage extends BufferedImage {
-
     /**
      *
      */
     private static final long serialVersionUID = 2739941470855574089L;
+    private int pixelCountMax = 5;
+    private int squarepixelCountMax = 25;
 
     public ECBufferedImage(BufferedImage read) {
         this(read.getWidth(), read.getHeight(), read.getType());
@@ -74,4 +87,52 @@ public class ECBufferedImage extends BufferedImage {
         return s;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ECBufferedImage that = (ECBufferedImage) o;
+
+        int diffPixels = -pixelCountMax;
+
+        for (int x = 0; x < getWidth(); x++)
+            for (int y = 0; y < getHeight(); y++) {
+                int thatRGB = that.getRGB(x, y);
+                int thisRGB = this.getRGB(x, y);
+                int[] thatRgba = new int[4];
+                int[] thisRgba = new int[4];
+                getRGBA(thatRGB, thatRgba);
+                getRGBA(thisRGB, thisRgba);
+
+                for (int i = 0; i < 4; i++) {
+                    pixelCountMax += thatRgba[i] - thisRgba[i];
+                }
+            }
+
+        if (pixelCountMax != that.pixelCountMax) return false;
+        return squarepixelCountMax == that.squarepixelCountMax;
+
+    }
+
+    private void getRGBA(int rgba, int[] componentsRGBA) {
+        int a = (rgba & 0xFF000000) >> 24;
+        int r = (rgba & 0xFF000000) >> 16;
+        int g = (rgba & 0xFF000000) >> 8;
+        int b = (rgba & 0xFF000000) >> 0;
+
+        componentsRGBA[0] = a;
+        componentsRGBA[1] = r;
+        componentsRGBA[2] = g;
+        componentsRGBA[3] = b;
+
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = pixelCountMax;
+        result = 31 * result + squarepixelCountMax;
+        return result;
+    }
 }
