f3ae518b9060267a059eff86475668a87cfe8524
Manuel Dahmen <manuel.dahmen@gmail.com>
2016-12-04 02:08:03 +0100

    Start copyright notices

--- src/test/java/be/manudahmen/emptycanvas/library/ZBufferImplTest.groovy
+++ src/test/java/be/manudahmen/emptycanvas/library/ZBufferImplTest.groovy
@@ -12,13 +12,12 @@
 
 package be.manudahmen.emptycanvas.library
 
-import be.manudahmen.emptycanvas.library.empty3.library.object.Camera
-import be.manudahmen.emptycanvas.library.empty3.library.object.Point3D
-import be.manudahmen.emptycanvas.library.empty3.library.object.ZBufferFactory
+import be.manudahmen.emptycanvas.library.empty3.library.object.*
+
+import java.awt.*
 
 class ZBufferImplTest extends groovy.util.GroovyTestCase {
-    def void testImageSizeGSZBufferBase() {
-        def Number
+    def void testImageSizeGSZBufferBaseDeliveredByZBufferFactory() {
         def Number laPres = 640
         def Number haPres = 480
 
@@ -30,9 +29,86 @@ class ZBufferImplTest extends groovy.util.GroovyTestCase {
         assertTrue(la == 640)
         assertTrue(ha == 480)
 
+        def resx = 1024
+        def resy = 768
+
+
+
         def Camera camera
         camera = new Camera(new Point3D(0, 0, 0), new Point3D(0, 0, 1))
         camera.calculerMatrice()
+
+        z.camera(camera)
+
         assertNotNull(camera)
     }
+
+    @Override
+    void setUp() {
+        super.setUp()
+
+    }
+
+    @Override
+    void tearDown() {
+        super.tearDown();
+    }
+
+    void testSinglePointAB() {
+        ZBuffer z = ZBufferFactory.instance(640, 480)
+
+
+        Camera c = new Camera(new Point3D(0, 0, 0), new Point3D(0, 0, 1))
+
+        c.calculerMatrice()
+
+        z.camera(c)
+
+        Scene sc = new Scene()
+
+        sc.add(new Point3D(0, 0, 0, new ColorTexture(Color.RED)))
+
+        z.scene(sc)
+
+        z.dessinerSilhouette3D()
+
+        def ECBufferedImage eCBufferedImage = z.image()
+
+
+        z.scene(new Scene())
+
+        z.plotPoint(new Point3D(0, 0, 0), Color.RED)
+
+        assertEquals(new Color(eCBufferedImage.getRGB(320, 240)), Color.RED)
+
+    }
+
+    void testCameraDefault() {
+        ZBuffer z = ZBufferFactory.instance(640, 480)
+
+        //Camera c = new Camera(new Point3D(0,0,0), new Point3D(0,0,1))
+
+        //c.calculerMatrice()
+
+        //z.camera(c)
+
+        Scene sc = new Scene()
+
+        sc.add(new Point3D(0, 0, 0, new ColorTexture(Color.RED)))
+
+        z.scene(sc)
+
+        z.dessinerSilhouette3D()
+
+        def ECBufferedImage eCBufferedImage = z.image()
+
+
+        z.scene(new Scene())
+
+        z.plotPoint(new Point3D(0, 0, 0), Color.RED)
+
+        assertEquals(new Color(eCBufferedImage.getRGB(320, 240)), Color.RED)
+
+    }
+
 }
