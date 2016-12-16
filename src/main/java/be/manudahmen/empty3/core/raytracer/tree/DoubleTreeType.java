196180f0e03d8f053ea26660107e7ef91ff143f5
Manuel Dahmen <manuel.dahmen@gmail.com>
2016-12-16 06:21:53 +0100

    New language algebra Test

--- src/main/java/be/manudahmen/empty3/core/raytracer/tree/DoubleTreeType.java
+++ src/main/java/be/manudahmen/empty3/core/raytracer/tree/DoubleTreeType.java
@@ -1,7 +1,10 @@
 package be.manudahmen.empty3.core.raytracer.tree;
 
+import be.manudahmen.empty3.Point3D;
+
 /**
  * Created by mary on 15-12-16.
  */
 public class DoubleTreeType extends TreeType {
+    private final Class doubleClass = Double.class;
 }
--- src/main/java/be/manudahmen/empty3/core/raytracer/tree/MatrixTreeType.java
+++ src/main/java/be/manudahmen/empty3/core/raytracer/tree/MatrixTreeType.java
@@ -1,7 +1,11 @@
 package be.manudahmen.empty3.core.raytracer.tree;
 
+import be.manudahmen.empty3.Matrix33;
+import be.manudahmen.empty3.Point3D;
+
 /**
  * Created by mary on 15-12-16.
  */
 public class MatrixTreeType extends TreeType {
+    private final Class MatrixClass = Matrix33.class;
 }
