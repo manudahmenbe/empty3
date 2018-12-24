e4f683271c46f41ac98b5ce06608007dc962fafd
Manuel Dahmen <manuel.dahmen@gmx.com>
2018-12-24 08:14:16 +0100

    Sphere et Citron

--- src/main/java/be/manudahmen/empty3/core/export/ObjExport.java
+++ src/main/java/be/manudahmen/empty3/core/export/ObjExport.java
@@ -1,25 +1,4 @@
-/*
- * Copyright (c) 2017. Tous les fichiers dans ce programme sont soumis à la License Publique Générale GNU créée par la Free Softxware Association, Boston.
- * La plupart des licenses de parties tièrces sont compatibles avec la license principale.
- * Les parties tierces peuvent être soumises à d'autres licenses.
- * Montemedia : Creative Commons
- * ECT : Tests à valeur artistique ou technique.
- * La partie RayTacer a été honteusement copiée sur le Net. Puis traduite en Java et améliorée.
- * Java est une marque de la société Oracle.
- *
- * Pour le moment le programme est entièrement accessible sans frais supplémentaire. Get the sources, build it, use it, like it, share it.
- */
 
-/**
- * *
- * Global license : * GNU GPL v3
- * <p>
- * author Manuel Dahmen <manuel.dahmen@gmail.com>
- * <p>
- * Creation time 25-oct.-2015 SURFACE D'ÉLASTICITÉ DE FRESNEL Fresnel's
- * elasticity surface, Fresnelsche Elastizitätfläche
- * http://www.mathcurve.com/surfaces/elasticite/elasticite.shtml *
- */
 package be.manudahmen.empty3.core.export;
 
 import be.manudahmen.empty3.*;
