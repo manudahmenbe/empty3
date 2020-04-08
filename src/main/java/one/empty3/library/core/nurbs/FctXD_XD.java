dae74e743c805f392094d9a92667076aafb62ac4
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-04-08 21:11:37 +0200

    Update FctXD_XD.java
    f nd

--- src/main/java/one/empty3/library/core/nurbs/FctXD_XD.java
+++ src/main/java/one/empty3/library/core/nurbs/FctXD_XD.java
@@ -1,2 +1,55 @@
+package one.empty3.library.core.nurbs;
 public class FctXD_XD {
+/*
+ *  This file is part of Empty3.
+ *
+ *     Empty3 is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Empty3 is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Empty3.  If not, see <https://www.gnu.org/licenses/>. 2
+ */
+
+/*
+ * This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+
+/**
+ * Created by md 2020 4 8 */
+public abstract class FctXD_XD {
+int dima,dimb;
+    public FctXD_XD(int dima, int dimb){
+this.dima = dima;
+this.dimb = dimb;
+}
+    public class Ixx extends FctXD_XD
+    {
+
+        @Override
+        public double result(Double [] input, Double [] output) {
+            return output;
+        }
+    }
+    public abstract double result(double [] input, double [] output);
+}
+
 }
