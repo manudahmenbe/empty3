789beab683ddda851b82e3139a955a9cc912d222
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-04-09 14:04:21 +0200

    Update F.java

--- src/main/java/one/empty3/library/F.java
+++ src/main/java/one/empty3/library/F.java
@@ -70,7 +70,7 @@ dimb = Integer.parseInt(splits[2]);
 }
 try {
 treeF = new AlgebricTree(formula);
-} catch(Exception ex) {
+} catch(AlgebraicSyntaxException|Exception ex) {
       ex.printStackTrace();
 }
 
