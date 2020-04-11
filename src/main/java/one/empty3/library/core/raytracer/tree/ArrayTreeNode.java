0eaf92c7a6b124e91c7dfbc841564cafdbcc4386
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-04-11 22:55:17 +0200

    Create ArrayTreeNode.java

---
+++ src/main/java/one/empty3/library/core/raytracer/tree/ArrayTreeNode.java
@@ -0,0 +1,12 @@
+package one.empty3.library.core.raytracer.tree;
+import one.empty3.library.*.;
+public class ArrayTreeNode<T> extends TreeNode {
+    private StructureMatrix<T> list;
+
+    public ArrayTreeNode(Class t) {
+list = new StructureMatrix(1, t) ;
+} 
+    public boolean add(Object o) {
+t.add(o);
+} 
+} 
