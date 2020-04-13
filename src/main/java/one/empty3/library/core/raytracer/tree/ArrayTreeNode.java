8e838c3b2e702532866776eb7907c985ab8cd699
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-04-13 16:08:02 +0200

    Update ArrayTreeNode.java

--- src/main/java/one/empty3/library/core/raytracer/tree/ArrayTreeNode.java
+++ src/main/java/one/empty3/library/core/raytracer/tree/ArrayTreeNode.java
@@ -1,15 +1,16 @@
 package one.empty3.library.core.raytracer.tree;
 import one.empty3.library.*;
-public class ArrayTreeNode extends TreeNodeType {
+public class ArrayTreeNodeType extends TreeNodeType {
     private StructureMatrix list;
 // todo des que tu as un separateur tu dois recommener avant quil soit pas la
-    public ArrayTreeNode(Class t) {
+    public ArrayTreeNodeType(Class t) {
+super();
 list = new StructureMatrix(1, t) ;
 } 
     public ArrayTreeNode() {
          this(Double.class);
 } 
     public boolean add(Object o) {
-t.add(o);
+list.add(1, o);
 } 
 } 
