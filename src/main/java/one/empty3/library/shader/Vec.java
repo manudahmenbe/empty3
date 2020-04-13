3e7973ae72e5bbe00d54dd55db90b885a7ef93dc
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-04-13 11:56:19 +0200

    Create Vec.java

---
+++ src/main/java/one/empty3/library/shader/Vec.java
@@ -0,0 +1,10 @@
+package one.empty3.library.shader;
+
+public class Vec
+{
+private final int dims;
+private StructureMatrix <Vec> vec
+    = new StructureMatrix (1, Vec.class);
+    public Vec(Double... ) {} 
+    public Vec(Vec... ) {} 
+} 
