d8832291baf27512daeee5d8711dafc7a87362c6
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-04-17 21:17:15 +0200

    Update VecAlTree.java

--- src/main/java/one/empty3/library/shader/VecAlTree.java
+++ src/main/java/one/empty3/library/shader/VecAlTree.java
@@ -2,12 +2,18 @@ package one.empty3.library.shader;
 public class VecAlTree extends Vec {
     AlgebricTree tree;
     public VecAlTree(String formula) {
-        tree=new AlgebricTree(formula) ;
+        tree=new AlgebricTree(formula) 
+            ;
+        try {
+            tree.construct();
+          } catch(Throwable t) {
+        System.out.println ("error vecaltreecondtruct\n"+trre ) ;
+        } 
   	
     } 
  public void setParameter(String p, Double d) {
  tree.setParameter(p, d);
- } 
+ }
     public Double [] getValue() {
     return new Double [] {tree. eval() } ;
     } 
