b7dfdc37b52896bb3a3334ed84a5e8435853ada9
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-04-18 17:19:36 +0200

    Update VecStack.java

--- src/main/java/one/empty3/library/shader/VecStack.java
+++ src/main/java/one/empty3/library/shader/VecStack.java
@@ -32,6 +32,7 @@ public String getFormula() {
 
 public Double [] value() {
     for(int=0;i<numsIn.data1d.size(); i++) 
-    tree.setParameter("in("+i+ ") ", super.value()[i] ) 
+    tree.setParameter("in("+i+ ") ", super.value()[i]) ;
+    return super.value();
 } 
 } 
