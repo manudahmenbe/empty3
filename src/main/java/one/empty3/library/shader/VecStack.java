a33142830a6c415fcfaf914626c4271828692b67
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-04-18 15:53:04 +0200

    Create VecStack.java

---
+++ src/main/java/one/empty3/library/shader/VecStack.java
@@ -0,0 +1,18 @@
+package one.empty3.library.shader;
+
+public class VecStack {
+    private int [] numsIn;
+    private int [] numsOut;
+
+
+/***
+* @param in in[i:int] 
+* @param out out[i : int]
+* @param formula f(in, out) 
+*/
+    public VecStack(int [] in, int[] out, String formula) {
+
+} 
+public void addVecIn() {} 
+public void addVecOut() {} 
+} 
