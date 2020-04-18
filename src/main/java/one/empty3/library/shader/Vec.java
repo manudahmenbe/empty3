1f9b2b0269b9f8972d615da66548cb48d84b7a8e
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-04-18 09:02:16 +0200

    Update Vec.java

--- src/main/java/one/empty3/library/shader/Vec.java
+++ src/main/java/one/empty3/library/shader/Vec.java
@@ -67,7 +67,7 @@ return s;
             int i = 0; // TODO
             int j = 0;
             for(i=0; i<vec.data1d.size(); i++) {
-                Double [] d = (Double) vec. data1d. getElem(i). value() ;
+                Double [] d = (Double)( vec. data1d. getElem(i). value()) ;
                 for(Double a : d) {
                     da[j] = a;
                     j++;
