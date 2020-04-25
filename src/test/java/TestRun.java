f5b023dec6a9e638c8df22341e68614dabd13fbe
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-04-25 07:43:09 +0200

    Update TestRun.java

--- src/test/java/TestRun.java
+++ src/test/java/TestRun.java
@@ -1,5 +1,12 @@
 public class TestRun {
-    public TestRun() {
-
+    public static void runTest(TestObjet to) {
+        new Thread(to).start();
     } 
+
+public static void main(String [] args) {
+     String cl = ;  
+     Object t=  cl.newInstance () ;
+     runTest(Test(TestObjet ) t) ;
+
+} 
 } 
