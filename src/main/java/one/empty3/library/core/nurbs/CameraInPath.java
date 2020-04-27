a6b05648761c23e0f5b0dcbfc637aa96402d9a02
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-04-27 13:06:06 +0200

    Update CameraInPath.java

--- src/main/java/one/empty3/library/core/nurbs/CameraInPath.java
+++ src/main/java/one/empty3/library/core/nurbs/CameraInPath.java
@@ -84,9 +84,9 @@ double t = this. t. getElem() ;
 
     @Override
     public Point3D calculerCurveT( double u, double t) {
-        t.setElem(t);
+        t.setElem((Double) t);
         calculerMatrice(null);
-        return courbe.getElem().calculerPoint3D(t.getElem());
+        return courbe.getElem().calculerPoint3D((Double) t.getElem());
     }
     @Override
     public Point3D calculerPointDansRepere(Point3D p) {
