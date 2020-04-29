1263552091dfe810c53c66084afff84c0259504e
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-04-29 22:03:58 +0200

    Update TracerD.java

--- src/main/java/one/empty3/library/core/nurbs/TracerD.java
+++ src/main/java/one/empty3/library/core/nurbs/TracerD.java
@@ -2,6 +2,10 @@ package one.empty3.library.core.nurbs;
 
 public class TracerD {
      /***
+     * distorted
      * d[u, v, +1, +1]
-     double [][][] d; 
+     * [1-n, 1-n]
+     * []
+     */
+     double [][][] distorted; 
 }
