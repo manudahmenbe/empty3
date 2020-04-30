d9f1edaef13ae49d8f53f45a440ba358f46fad40
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-04-30 21:46:44 +0200

    Create PolyCurve.java

---
+++ src/main/java/one/empty3/library/core/nurbs/PolyCurve.java
@@ -0,0 +1,14 @@
+package one.empty3.library.core.nurbs;
+import one.empty3.library.*;
+
+
+/***
+* Todo pointscurve, tangent (c1)
+*/
+public class PolyCurve extends ParametricCurve {
+    private StructureMatrix<ParamtricCurve> curve;
+    private StructureMatrix<Double> us;
+    private StructureMatrix<Double> vs;
+    public PolyCurve() {
+    }
+}
