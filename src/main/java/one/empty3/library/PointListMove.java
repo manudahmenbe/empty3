d66bffc82c04290243fdd58299e39b829b5f93ab
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-05-02 16:43:33 +0200

    Update PointListMove.java

--- src/main/java/one/empty3/library/PointListMove.java
+++ src/main/java/one/empty3/library/PointListMove.java
@@ -1,29 +1,30 @@
 package one.empty3.library;
 import one.empty3.library.core.nurbs.*;
 public class PointListMove {
-    private List<Point3D> points;
+    private List<RepresentableT> points;
     private StructureMatrix<String> formula = new StructureMatrix (1, String.class);
     private StructureMatrix<ParametricCurve> curves = new StructureMatrix (1, ParametricCurve.class);
     private int type;
     private double t, dt;
     // Slices of ord.ex (0,1), curve, curveOp 
     // tan, norm, cross, add... ???
-    public PointListMove (List<Point3D> p, double t, double dt) {
+    // collide
+    public PointListMove (List<RepresantableT> p, double t, double dt) {
         this.points = p;
         this.t = t;this.dt = dt;
     }
     
-    public void random() {
+    public void random(String property) {
     
     }
-    
-    public void curve(ParametricCurve curve) {
+
+    public void curve(String property, ParametricCurve curve) {
         this.curve = curve;
     }
     /***
      * @param formula vec{3}(f(ord(0), ord(1), ...))
      */
-    public void formulaOrd(String formula) {
+    public void formulaOrd(String property, String formula) {
        this.formula = formula;
     
     }
