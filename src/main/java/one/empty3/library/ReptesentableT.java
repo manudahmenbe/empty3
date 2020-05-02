3056929c0dba3540dd7f0de4306748a5b0bc25c9
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-05-02 17:54:05 +0200

    Update ReptesentableT.java

--- src/main/java/one/empty3/library/ReptesentableT.java
+++ src/main/java/one/empty3/library/ReptesentableT.java
@@ -1,7 +1,11 @@
 package one.empty3.library;
 
-public class RepresentableT extends Representable{
+public class RepresentableT {
     protected double t;
-  
-
+    public double getT() {
+         return t;
+    }
+    public void setT(double t) {
+         this.t = t;
+    }
 }
