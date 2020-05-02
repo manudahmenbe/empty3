b91effb52c1137bf48d2c91b1078d01193750451
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-05-02 18:00:00 +0200

    Update ReptesentableT.java

--- src/main/java/one/empty3/library/ReptesentableT.java
+++ src/main/java/one/empty3/library/ReptesentableT.java
@@ -1,11 +1,16 @@
 package one.empty3.library;
 
 public class RepresentableT {
-    protected double t;
+    protected double t, dt;
+    private ListPointMove propertiesMoves;
     public double getT() {
          return t;
     }
     public void setT(double t) {
          this.t = t;
+    }
+    
+    public void next() {
+         propertiesMoves.next();
     }
 }
