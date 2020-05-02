e720f006b3f85fe6d8eda6a942521951fc9de7bc
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-05-02 09:58:24 +0200

    Update TestVoronoi.java

--- src/main/java/test3/TestVoronoi.java
+++ src/main/java/test3/TestVoronoi.java
@@ -1,14 +1,20 @@
 package test3;
 import one.empty3.library.core.testing.*;
 import one.empty3.library.*;
+import java.awt.Colir;
+import java.util.List;
 public class TestVoronoi extends TestObjet {
 Double [][] distancesSum;
+   private List<Point3D> pointsList;
 Double maxDist;
 
    public void ginit() {
+        pointsList = new ArrayList();
         maxDist = 0.0;
         distancesSum = new Double [getResx()][getResy()];
-
+        pointsList.add(new Point3D(Math.random()*getResx(),
+                                  Math.random()*getResy(),
+                                  0.0));
         
    }
 
@@ -27,11 +33,12 @@ Double maxDist;
         for(int i= 0;i<getResx(); i++)
              for(int j= 0;j<getResy(); j++) {
 distancesSum[i][j] /= maxDist;
+                }
 // Laplace derivation 2
  for(int i= 0;i<getResx(); i++)
              for(int j= 0;j<getResy(); j++) {
                  // laplacien[i][j]
-}
+             }
    }
 
 
