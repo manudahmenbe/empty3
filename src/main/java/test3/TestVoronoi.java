8d4534bfa043eb2452842b260cf304e5d9cf0f04
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-05-02 08:45:01 +0200

    Update TestVoronoi.java

--- src/main/java/test3/TestVoronoi.java
+++ src/main/java/test3/TestVoronoi.java
@@ -2,5 +2,12 @@ package test3;
 import one.empty3.library.core.testing.*;
 import one.empty3.library.*;
 public class TestVoronoi extends TestObjet {
-   
+Double [][] distancesSum;
+Double maxDist;
+
+   public TestVoronoi () {
+        maxDist = 0.0;
+        distancesSum = new Double [getResx()][getResy()];
+        
+   }
 }
