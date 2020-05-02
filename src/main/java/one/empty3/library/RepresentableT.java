d3c9d56a7d3e3dfeb372fa1df29f4971b2d8b814
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-05-02 19:34:50 +0200

    Update RepresentableT.java

--- src/main/java/one/empty3/library/RepresentableT.java
+++ src/main/java/one/empty3/library/RepresentableT.java
@@ -3,10 +3,10 @@ package one.empty3.library;
 public class RepresentableT {
     protected double t, dt;
     private PointListMove propertiesMoves;
-    public double getTime() {
+    public double getTD() {
          return t;
     }
-    public void setTime(double t) {
+    public void setTD(double t) {
          this.t = t;
     }
     
