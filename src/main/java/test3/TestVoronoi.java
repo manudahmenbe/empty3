584838697844c804bd59428b9258d52cf08ca173
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-05-02 08:59:17 +0200

    Update TestVoronoi.java

--- src/main/java/test3/TestVoronoi.java
+++ src/main/java/test3/TestVoronoi.java
@@ -5,9 +5,25 @@ public class TestVoronoi extends TestObjet {
 Double [][] distancesSum;
 Double maxDist;
 
-   public TestVoronoi () {
+   public void ginit() {
         maxDist = 0.0;
         distancesSum = new Double [getResx()][getResy()];
         
    }
+
+   public void finit() {
+        maxDist = 0.0;
+        for(int i= 0;i<getResx(); i++)
+             for(int j= 0;j<getResy(); j++) {
+
+                  distancesSum [i][j] = 0.0;
+                  Point3D p = new Point3D((double)i, (double)j, 0.0);
+                  for(int k=0; k<pointsList.size(); k++)
+                       distancesSum[i][j] += Point3D.distance(p, pointsList.get(k));
+                  if(distancesSum[i][j]>maxDist)
+                       maxDist = distancesSum[i][j];
+   }
+
+
+   public void testScene;
 }
