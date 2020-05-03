37cf4f35b2337ff4d7e8e7c5323d798c7de5ca8a
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-05-03 19:56:52 +0200

    Update Colors.java

--- src/main/java/one/empty3/library/core/lighting/Colors.java
+++ src/main/java/one/empty3/library/core/lighting/Colors.java
@@ -61,6 +61,7 @@ public class Colors {
       */
     public static Color mean(Color[] c, double[] d, double norm) {
       int compNo = 3;
+        if(c==null || d==null || c.length!=d.length)
         float [] r = new float[compNo];
         float [] f = new float[compNo];
         float sum = 0f;
