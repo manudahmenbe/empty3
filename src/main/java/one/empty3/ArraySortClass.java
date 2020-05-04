240c734142b7eff17b370eea94d05f1ced1d1e21
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-05-04 09:48:45 +0200

    Update ArraySortClass.java

--- src/main/java/one/empty3/ArraySortClass.java
+++ src/main/java/one/empty3/ArraySortClass.java
@@ -1,6 +1,6 @@
 package one.empty3;
 public class ArraySortClass<T> {
-    public static void sortArray(int [] cord, T [] array, Comparable [] values) {
+    public void sortArray(int [] cord, T [] array, Comparable [] values) {
     
     }
 }
