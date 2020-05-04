f6ec94ce9badab1ea9a26db14cfddf17b007e5df
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-05-04 08:24:22 +0200

    Create ColorDist.java

---
+++ src/main/java/one/empty3/library/core/lighting/ColorDist.java
@@ -0,0 +1,14 @@
+package one.empty3.library.core.lighting;
+
+public class ColorDist implements Comparable {
+        public Color color;
+        public double dist;
+        
+        public int compareTo(Object o) {
+            if (o instanceof ColorDist) {
+                ColorDist cd = (ColorDist) o;
+                return dist<cd.dist?-1:(dist==cd.dist?0:1);
+           } else 
+                return 0;//throw??
+        }
+    }
