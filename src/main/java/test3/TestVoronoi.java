2978a27e00fa401e204167cba29da6e49054df82
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-05-06 09:04:58 +0200

    Update TestVoronoi.java

--- src/main/java/test3/TestVoronoi.java
+++ src/main/java/test3/TestVoronoi.java
@@ -12,7 +12,7 @@ public int pointsSize = 30;
   // private Double [][][] distancesSum;
    //private ArrayList<Point3D> pointsList;
    private Double maxDist;
-   private int nPointsDist = 3;
+   private int nPointsDist = 5;
    ///private double[][][] pointDist;
   // private int[][][] pointNo;
    private int[][] colorsArr;
