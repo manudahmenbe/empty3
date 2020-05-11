4df565674d263f9e8e6e80a0a1a7d739bc320e64
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-05-11 14:07:22 +0200

    Update Pojo.java

--- src/main/java/Pojo.java
+++ src/main/java/Pojo.java
@@ -6,4 +6,25 @@ public class Pojo {
   public static Properties getProperties(Object o, Properties p) {
          
     }
+    
+    public Class getPropertyType(String propertyName) throws NoSuchMethodException {
+        Method propertyGetter = null;
+        propertyGetter = this.getClass().getMethod("get" + ("" + propertyName.charAt(0)).toUpperCase() + (propertyName.length() > 1 ? propertyName.substring(1) : ""));
+        return propertyGetter.getReturnType();
+    }
+
+    public void setProperty(String propertyName, Object value) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+        Method propertySetter = null;
+
+        propertySetter = this.getClass().getMethod("set" + ("" + propertyName.charAt(0)).toUpperCase() + (propertyName.substring(1)), value.getClass());
+        propertySetter.invoke(this, value);
+        System.out.println("RType : " + this.getClass().getName() + " Property: " + propertyName + " New Value set " + getProperty(propertyName));
+    }
+
+    public Object getProperty(String propertyName) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+        Method propertySetter = null;
+        propertySetter = this.getClass().getMethod("get" + ("" + propertyName.charAt(0)).toUpperCase() + propertyName.substring(1));
+        return propertySetter.invoke(this);
+    }
+
 }
