ff692e6158a071dbae68d97a3b7a5cb7b85401f1
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-05-11 09:09:46 +0200

    Update TestRun.java

--- src/main/java/TestRun.java
+++ src/main/java/TestRun.java
@@ -5,14 +5,33 @@ public class TestRun {
     } 
 
      public static void main(String [] args) {
-try {
-          Class cl = Class.forName(args[0]);  
+
+System.out.println(args.length + " arguments :");
+    for (String arg: args) {
+         String kv = arg.split("=");
+         String key = kv[0];
+         String value = kv[1];
+
+         switch(key) {
+    case "class":
+         Class cl = Class.forName(args[0]);  
          
               Object t=  cl.newInstance () ;
-              if(t instanceof TestObjet) 
-                   runTest((TestObjet ) t) ;
+              
          } catch(Exception ex) {
               ex.printStackTrace();
          }
+         break;
+    case "resx":
+    case "resy":
+         break;
+    }
+
+
+    if(t instanceof TestObjet) 
+                   runTest((TestObjet ) t) ;
+}
+
+    }
      } 
 } 
