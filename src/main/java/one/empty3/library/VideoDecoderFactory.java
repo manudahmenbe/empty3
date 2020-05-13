dae82bb48a29e64f24e097d11727181f05ee6f76
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-05-13 13:13:40 +0200

    Update VideoDecoderFactory.java

--- src/main/java/one/empty3/library/VideoDecoderFactory.java
+++ src/main/java/one/empty3/library/VideoDecoderFactory.java
@@ -1,9 +1,9 @@
 package one.empty3.library;
 import java.io.File;
 public class VideoDecoderFactory {
-     public static VideoDecoder createInstance(File f, TextureMov m)
+     public static VideoDecoderMontemedia createInstance(File f, TextureMov m)
       {
-          return new DecodeAndCaptureFramesXuggle(f,m);
+          return new DecodeMonte(f,m);
        }
 
 
