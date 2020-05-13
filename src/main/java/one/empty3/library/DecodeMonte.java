ed11efea765b5cce4d6170b4bf84840c362d3c20
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-05-13 22:08:51 +0200

    Update DecodeMonte.java

--- src/main/java/one/empty3/library/DecodeMonte.java
+++ src/main/java/one/empty3/library/DecodeMonte.java
@@ -18,7 +18,7 @@ public class DecodeMonte extends VideoDecoder {
     if(!file.exists())
        throw new NullPointerException("file not found exception"+file.getCanonicalPath());
  MovieReader in = Registry.getInstance().getReader(file);
-if(!file.exists())
+if(in==null)
       throw new NullPointerException("in moviereader"+file.getCanonicalPath());
  //ArrayList<BufferedImage> frames=new ArrayList<BufferedImage> ();
  
