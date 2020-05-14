35b2f0b06d7c66975e080003d9f5b86b34049c97
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-05-14 06:41:13 +0200

    Update DecodeMonte.java

--- src/main/java/one/empty3/library/DecodeMonte.java
+++ src/main/java/one/empty3/library/DecodeMonte.java
@@ -229,4 +229,66 @@ if(imgBuf.size()>MAXSIZE)
             }
         }
 }
+ 
+ 
+             }
+        }
+    }
+
+    /**
+     * Creates a buffered image of the specified depth with a random color palette.
+     */
+    private static BufferedImage createImage(Format format) {
+        int depth = format.get(DepthKey);
+        int width = format.get(WidthKey);
+        int height = format.get(HeightKey);
+        PixelFormat pixelFormat = format.get(PixelFormatKey);
+
+        Random rnd = new Random(0); // use seed 0 to get reproducable output
+        BufferedImage img;
+        switch (depth) {
+            case 24:
+            default: {
+                img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+                break;
+            }
+            case 8:
+                if (pixelFormat == PixelFormat.GRAY) {
+                    img = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
+                    break;
+                } else {
+                    byte[] red = new byte[256];
+                    byte[] green = new byte[256];
+                    byte[] blue = new byte[256];
+                    for (int i = 0; i < 255; i++) {
+                        red[i] = (byte) rnd.nextInt(256);
+                        green[i] = (byte) rnd.nextInt(256);
+                        blue[i] = (byte) rnd.nextInt(256);
+                    }
+                    rnd.setSeed(0); // set back to 0 for reproducable output
+                    IndexColorModel palette = new IndexColorModel(8, 256, red, green, blue);
+                    img = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_INDEXED, palette);
+                    break;
+                }
+            case 4:
+                if (pixelFormat == PixelFormat.GRAY) {
+                    img = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
+                    break;
+                } else {
+                    byte[] red = new byte[16];
+                    byte[] green = new byte[16];
+                    byte[] blue = new byte[16];
+                    for (int i = 0; i < 15; i++) {
+                        red[i] = (byte) rnd.nextInt(16);
+                        green[i] = (byte) rnd.nextInt(16);
+                        blue[i] = (byte) rnd.nextInt(16);
+                    }
+                    rnd.setSeed(0); // set back to 0 for reproducable output
+                    IndexColorModel palette = new IndexColorModel(4, 16, red, green, blue);
+                    img = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_INDEXED, palette);
+                    break;
+                }
+        }
+        return img;
+    }
 }
