327a4676a7b92fb294aaaeeaeeee54391423696b
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-05-15 03:39:08 +0200

    Update ShaderTexture.java

--- src/main/java/one/empty3/library/ShaderTexture.java
+++ src/main/java/one/empty3/library/ShaderTexture.java
@@ -2,6 +2,9 @@ package one.empty3.library;
 
 import one.empty3.library.shader;
 
-public class ShaderTexture extends ITexture {
+public class ShaderTexture extends TextureMov {
+     public ShaderTexture(String glsl) {
+     }
+     
 
 }
