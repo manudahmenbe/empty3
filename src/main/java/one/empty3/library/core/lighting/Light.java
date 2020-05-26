716d18fc0bf2f4c7904a6dab03a4164b2a6ba075
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-05-26 08:33:42 +0200

    Update Light.java

--- src/main/java/one/empty3/library/core/lighting/Light.java
+++ src/main/java/one/empty3/library/core/lighting/Light.java
@@ -1,4 +1,21 @@
 package one.empty3.library.core.lighting;
 public abstract class Light {
+      public int [] getColorArray(int c) {
+          return new int[] {
+              c&0xff000000>>24,
+              c&0x00ff0000>>16,
+              c&0x0000ff00>>8
+              c&0x000000ff>>0
+            }
+      }
+      public int getColorInt(int [] colors) {
+           return colors[3]<<24+colors[2]<<16+
+              colors[1]>>8+colors[0];
+      }
       public abstract Light();
+
+      public int compColor(int c, Point3D p, Point3D n, Camera c)) {
+           return c;
+
+      }
 }
