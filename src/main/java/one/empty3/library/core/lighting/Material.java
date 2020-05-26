407ef1b89492ed9a71d9441062ba41b43aef6b68
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-05-26 09:49:18 +0200

    Create Material.java

---
+++ src/main/java/one/empty3/library/core/lighting/Material.java
@@ -0,0 +1,12 @@
+public class Material {
+     double Ka, Kd, Ks;
+     Representable r;
+     public Material(double Ka, double Kd, double Ks, Representable r) {
+          this.r = r;
+          this.Ka = Ka;
+          this.Kd = Kd;
+          this.Ks = Ks;
+     }
+
+
+}
