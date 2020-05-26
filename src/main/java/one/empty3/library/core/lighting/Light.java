f8c1f1d903a2b46c92502f72a4bb03e3ce503343
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-05-26 10:34:54 +0200

    Update Light.java

--- src/main/java/one/empty3/library/core/lighting/Light.java
+++ src/main/java/one/empty3/library/core/lighting/Light.java
@@ -50,7 +50,7 @@ Point3D eye = c.eye();
            Point3D s = (source.moins(p).norme1());
            Point3D v = (c.eye().moins(p).norme1())
            Point3D Id =  cp.mult(Ld.*m.getKd()*(s.dot(n)));
-           Point3D r = s.mult(-1).plus(n.mult(2*s.dot(n));
+           Point3D r = s.mult(-1).plus(n.mult(2*s.dot(n)));
            
            Point3D Is = cp.mult( Ls*m.getKs()*Math.pow((r.dot.v), f));
            return getColor(new int[] {(float)cp.get(3), (float)cp.get(2),(float)cp.get(1),(float)cp.get(0)});;
