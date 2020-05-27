ae6029323b29a31018d9819e3651596ee1f6e8e9
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-05-27 09:43:49 +0200

    Update DoubleArray.java

--- src/main/java/one/empty3/library/DoubleArray.java
+++ src/main/java/one/empty3/library/DoubleArray.java
@@ -1,12 +1,24 @@
 package one.empty3.library;
 import java.util.List;
 public class DoubleArray {
-    List<Double[]> doubles;
-    List<Integer> index;
-    List<Integer> free;
+    private int length = 1000*1000*1000;
+    List<int[]> doubles;
+    List<int> index;
+    List<int> location;
+    int max = 0;
     public DoubleArray(){
         doubles = new ArrayList<>();
-        doubles.add(new Double[1000*1000*1000]);
+        doubles.add(new Double[length]);
+        index = new ArrayList<>();
+        index.add(new int[length]);
+      //  location = new ArrayList<>()
+    }
+    public Double getIndex(int index) {
+        return doubles.get(0)[this.index.get(0)[index]];
+    }
     
+    public int add(Double d) {
+        return doubles.get(0)[this.index.get(0)[max] = max] = d;
+        max++;
     }
 }
