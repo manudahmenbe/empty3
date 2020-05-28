7f9f0b1c2c65e53b445e61ff3271bd8db68b1ae2
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-05-28 16:49:42 +0200

    Update DoubleArray.java

--- src/main/java/one/empty3/library/DoubleArray.java
+++ src/main/java/one/empty3/library/DoubleArray.java
@@ -8,15 +8,33 @@ public class DoubleArray {
     private int length = 1000*1000*1000;
     List<Double[]> doubles;
     List<Integer[]> index;
+    int spaces;
+    boolean [] freeSpace
     int max = 0;
-    public DoubleArray(){
+    public DoubleArray(int spaces, int spaceLength){
+        freeSpace = new boolean[spaces];
+        this.spaces = spaces
         doubles = new ArrayList<>();
-        doubles.add(new Double[length]);
+        doubles.add(new Double[spaceLength]);
         index = new ArrayList<>();
-        index.add(new Integer[length]);
+        index.add(new Integer[spaceLength]);
       //  location = new ArrayList<>()
     }
-    public void clear() {}
+    public void clear() {
+         for(int i=0; i<index.size(); i++) {
+             int j = 0;
+             boolean isEmpty = true;
+             while(j<index.get(i).length) {
+                 if(index.get(i)[j]!=0) {
+                     isEmpty = false;
+                     break;
+                 }
+             }
+             if(isEmpty) { 
+                 freeSpace[i] = true;
+             
+         }
+    }
     public Double getDouble(int index) {
         return doubles.get(0)[this.index.get(0)[index]];
     }
