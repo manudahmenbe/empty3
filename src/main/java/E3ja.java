1d2b7307c70bd6cd041f5f7afa97d48127f79255
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-05-30 10:52:35 +0200

    Update E3ja.java

--- src/main/java/E3ja.java
+++ src/main/java/E3ja.java
@@ -2,6 +2,19 @@ public class E3ja {
      public E3ja(Object object) {
           
      }
-     public void setFunction(String path, String function) {
+     /*** 
+       *  example: setFormulaFunctionA(AlgebricTree al) { ... }
+       *           setFormulaFunctionB(...
+       *  "formulaFunctionA=sin(x):formulaFunctionB=cos(x)"
+     public void setFunctions(String propertyFunction) {
+          String [] pf =  path.split(":");
+          for(String i : pf) {
+               String [] pair = i.split("=");
+               String propertyName = pair[0];
+               String formula = pair[1];
+               AlgebricTree tree = new AlgebricTree(formula);
+               Pojo.setProperty(propertyName, tree);
+               
+          }
      }
 }
