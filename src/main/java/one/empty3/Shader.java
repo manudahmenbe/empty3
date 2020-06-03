d9593052d7e3d013ae76c33f00d8804a14838aad
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-06-03 14:46:53 +0200

    Update Shader.java

--- src/main/java/one/empty3/Shader.java
+++ src/main/java/one/empty3/Shader.java
@@ -1,6 +1,7 @@
 package one.empty3;
 import java.io.File;
 import java.awt.image.BufferedImage;
+import java.util.*
 /***
  * vn(int n)
  * vn(vn...)
@@ -10,17 +11,64 @@ import java.awt.image.BufferedImage;
   * where the rapers shit go?
  */
 public class Shader{
+   class SymbolTreeNode {
+        String name;
+        Items kind;
+        SymbolTreeNode children;
+ } 
+ class Tree {
+  SymbolTreeNode root;
+  Iterator<SymbolTreeNode> iterate(){return null;}
+
     class enum Items {Comment, Macro, FunctionDeclaration, FunctionBody, MemberVariable, ClassDeclaration, VariableName, VariableType, Literal, Scalar, ClassName}
-    class Comment {
+    class Comment extends SymbolTreeNode{
      enum Type { Line, Block, Doc }
      String text;
     }
-    class Macro { 
+    class Macro extends SymbolTreeNode{ 
         enum Type { Macro, Include }
         String name;
         String def;
     }
-    claaa 
+    class Class extends SymbolTreeNode{}
+ 
+ 
+    public boolean splitInTypes(String shStr) {
+     int i = 0;
+     int posTry = -1;
+         while(i<shStr.length) {
+             char ch = shStr.charAt(i);
+             if(ch=='#') {i = readMacro(shStr, i+1);continue;}
+             do { 
+                 i = readKeyword(shStr, i); 
+              } while(i>=0)
+             i = readType(chStr, i) ;
+             i = readIdentifier(shStr, i);
+             
+              
+             
+              
+              
+             
+             switch(stack.current.type) {
+               case Items.Class:
+                   i = parseExtend(chStr);
+                   break;
+               case Items.Function:
+                   i = parseArgumentList(shStr, i) ;
+                   break;
+               case Items.Variable:
+                   i = parseExpr(chStr) ;
+                   break;
+               
+              } 
+                
+
+             
+
+         }
+     return true;
+    }
     List<String> shaderFiles = new ArrayList<>();
     
     public Shader(File fileOrDirectory) {
