743445ef25bc69e0a7652e97f56929074d479c10
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-06-04 14:41:49 +0200

    Update Shader.java

--- src/main/java/one/empty3/Shader.java
+++ src/main/java/one/empty3/Shader.java
@@ -46,25 +46,24 @@ public class Shader{
     public int readChar(String shStr, char c, int i) {
         return sh.charAt(i)==c?i+1:i;
     }
-    public int readComment(String shStr , int i){
+    public int readComment(String shStr , int i) {
         if(shStr.charAt(i)=='/') {
             i++;
-            if(shStr.charAt(i)=='/')
-                while(shStr.charAt(i)!='\n')
+            if(shStr.charAt(i)=='/') {
+                while(shStr.charAt(i)!=System.lineSeparator)
                     i++;
-            if(shStr.charAt(i)=='/')
-                while(shStr.charAt(i)!='*') {
+                return i;
+             }
+            if(shStr.charAt(i)=='*') {
+                i++;
+                while(i<shStr.length()-1 && shStr.charAt(i)!='*' && shStr.charAt(i+1)=='/') {
                     i++;
                 }
-             i++;
-            if(shStr.charAt(i)!='/') {
-               i++;
-               root.children.add(new Comment());
-               return i;
+                i++;
+                return i;
             }
-            
         }
-        return -1;
+        return i;
     }
     
     class Literal {
@@ -83,19 +82,21 @@ public class Shader{
          }
          return i;
      }
-         public int parseInt(shStr, int i){
+     public int parseInt(shStr, int i){
           return i;
 
          }
-         public int parseFloat(shStr, int i){
+     public int parseFloat(shStr, int i){
           return i;
           
          }
-         public int parseDouble(shStr, int i){
+     public int parseDouble(shStr, int i){
           return i;
           
-         }
-         public int readContainer(String shStr, int i) {}
+     }
+    public int readContainer(String shStr, int i) {
+         return i;
+    }
     public int readLiteral(String shStr, int i) {
          i = parseString(shStr, i);
          i = parseInt(shStr, i);
