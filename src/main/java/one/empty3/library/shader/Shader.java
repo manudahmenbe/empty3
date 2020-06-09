2ea855be0f323b9d348cc2b22c194e5d4f058737
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-06-09 21:59:45 +0200

    Update Shader.java

--- src/main/java/one/empty3/library/shader/Shader.java
+++ src/main/java/one/empty3/library/shader/Shader.java
@@ -164,9 +164,9 @@ public class Shader{
      
          
         int i= 0;
-        
+        int j - -1;
         while(j!=i) {
-            int j= i;
+            j = i;
             j = readBlank(shStr, i);
             if(j==i)
                 j = readComment(shStr, i);
