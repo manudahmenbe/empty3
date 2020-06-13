88e9733db7884d635aa3e1d4f992c37fed816db3
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-06-13 22:31:46 +0200

    Update Shader.java

--- src/main/java/one/empty3/library/shader/Shader.java
+++ src/main/java/one/empty3/library/shader/Shader.java
@@ -10,29 +10,29 @@ import java.util.*;
   * glsl light. like.
   *
  * chaque token doit etre identifie a
- * un type
+ * un type et plus
  */
 
 public class Shader{
     String keywords = "attribute uniform varying layout centroid flat smooth noperspective patch sample subroutine in out inout invariant discard mat2 mat3 mat4 dmat2 dmat3 dmat4 mat2x2 mat2x3 mat2x4 dmat2x2 dmat2x3 dmat2x4 mat3x2 mat3x3 mat3x4 dmat3x2 dmat3x3 dmat3x4 mat4x2 mat4x3 mat4x4 dmat4x2 dmat4x3 dmat4x4 vec2 vec3 vec4 ivec2 ivec3 ivec4 bvec2 bvec3 bvec4 dvec2 dvec3 dvec4 uvec2 uvec3 uvec4 lowp mediump highp precision sampler1D sampler2D sampler3D samplerCube sampler1DShadow sampler2DShadow samplerCubeShadow sampler1DArray sampler2DArray sampler1DArrayShadow sampler2DArrayShadow isampler1D isampler2D isampler3D isamplerCube isampler1DArray isampler2DArray usampler1D usampler2D usampler3D usamplerCube usampler1DArray usampler2DArray sampler2DRect sampler2DRectShadow isampler2DRect usampler2DRect samplerBuffer isamplerBuffer usamplerBuffer sampler2DMS isampler2DMS usampler2DMS sampler2DMSArray isampler2DMSArray usampler2DMSArray samplerCubeArray samplerCubeArrayShadow isamplerCubeArray usamplerCubeArray";
-String  vq =  "attribute const uniform varying" ;
-String centroid = "centroid";
-String loop =" break continue do for while" ;
-String ifelse = "if else";
- String param = "in out inout" ;
-String scalar =" float int void bool true false";
-String invariant = "invariant" ;
-String returns = "discard return";
-
-String mat = "mat2 mat3 mat4" ;
-String mat2 = "mat2x2 mat2x3 mat2x4";
-String mat3 = "mat3x2 mat3x3 mat3x4" ;
-String mat4 = "mat4x2 mat4x3 mat4x4";
-String vec = "vec2 vec3 vec4 ivec2 ivec3 ivec4 bvec2 bvec3 bvec4" ;
-String sampler = "sampler1D sampler2D sampler3D sammpler samplerCube";
- String sash = "sampler1DShadow sampler2DShadow" ;
-String struct = "struct";
- List<String> lines = new ArrayList<>();
+    String  vq =  "attribute const uniform varying" ;
+    String centroid = "centroid";
+    String loop =" break continue do for while" ;
+    String ifelse = "if else";
+    String param = "in out inout" ;
+    String scalar =" float int void bool true false";
+    String invariant = "invariant" ;
+    String returns = "discard return";
+    String fmath = "cross dot sum fract floor round distance min max exp pow sin cos tan acos asin atan";
+    String mat = "mat2 mat3 mat4" ;
+    String mat2 = "mat2x2 mat2x3 mat2x4";
+    String mat3 = "mat3x2 mat3x3 mat3x4" ;
+    String mat4 = "mat4x2 mat4x3 mat4x4";
+    String vec = "vec2 vec3 vec4 ivec2 ivec3 ivec4 bvec2 bvec3 bvec4" ;
+    String sampler = "sampler1D sampler2D sampler3D sammpler samplerCube";
+    String sash = "sampler1DShadow sampler2DShadow" ;
+    String struct = "struct";
+    List<String> lines = new ArrayList<>();
     enum DataType {Void, Scalar, Object}
     // Datadim?
     enum ParseContext {ClassDef, MemberDef, MethodDef, ListArgs, MethodBodyDef, VariableDef, Instruction, Operation }
