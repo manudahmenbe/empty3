e256e2e0f3530a8ea773bdca9962476608287292
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-06-14 11:24:11 +0200

    Update ShaderOperations.java

--- src/main/java/one/empty3/library/shader/ShaderOperations.java
+++ src/main/java/one/empty3/library/shader/ShaderOperations.java
@@ -1,1 +1,14 @@
-
+package one.empty3.library.shader;
+import one.empty3.library;
+public ShaderOperations {
+    class VecValue extends Vec;
+    class MatValue extends Mat;
+    class SamplerValue extends Sampler;
+    public ShaderOperations() {
+         
+    }
+  
+    public Object operationChoice(Object o1, Object o2, String... classes) {
+    }
+  
+}
