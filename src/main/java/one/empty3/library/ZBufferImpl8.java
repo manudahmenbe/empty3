7a4ba4ba44cabddff5e8eb77c1d110a1e2bae32d
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-06-14 21:47:08 +0200

    Update ZBufferImpl8.java

--- src/main/java/one/empty3/library/ZBufferImpl8.java
+++ src/main/java/one/empty3/library/ZBufferImpl8.java
@@ -155,7 +155,7 @@ public class ZBufferImpl8 extends Representable implements ZBuffer {
     public Box2D box;
     private Point3D activeLight = new Point3D(-10d, 0d, 100d);
     private int displayType = SURFACE_DISPLAY_TEXT_QUADS;
-    ZBufferthat;
+    ZBuffer that;
     Data data1;
     public ZBufferImpl8() {
         that = this;
