551d1a50c7393c80d5e0ae120322069c9210768b
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-06-14 11:32:06 +0200

    Update Mat.java

--- src/main/java/one/empty3/library/shader/Mat.java
+++ src/main/java/one/empty3/library/shader/Mat.java
@@ -1,1 +1,12 @@
-
+package one.empty3.library.shader;
+public class Mat {
+     int l,c;
+     public Mat(int i, int j) {
+         this.i = i;
+         this.j = j;
+     }
+     public boolean setValues( boolean isLineVec, double [] values) {
+     }
+     public boolean setValues(boolean isLineVec, Vec... vecs) {
+     }
+}
