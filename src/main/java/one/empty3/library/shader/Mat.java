6cf8a8031f07885061116bca17232b86190d1969
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-06-14 11:53:07 +0200

    Update Mat.java

--- src/main/java/one/empty3/library/shader/Mat.java
+++ src/main/java/one/empty3/library/shader/Mat.java
@@ -2,11 +2,32 @@ package one.empty3.library.shader;
 public class Mat {
      int l,c;
      public Mat(int i, int j) {
-         this.i = i;
-         this.j = j;
+         this.l = i;
+         this.c = j;
      }
      public boolean setValues( boolean isLineVec, double [] values) {
      }
      public boolean setValues(boolean isLineVec, Vec... vecs) {
+     }
+     public Vec slice(int l1, int c1, int l1, int c2) {
+          
+     }
+     public Vec slice(int l_c_2... line_column_args2n) {
+     }
+     public Vec product(Vec vec) {
+     }
+     public Mat product(Mat mat) {
+     }
+     public Mat dotProduct(Mat mat) {
+     }
+     public void product(double d) {
+     }
+     public Mat add(Mat mat) {
+     }
+     public void add(double d) {
+          }
+     public Mat transpose() {
+          }
+     public double determinant() {
      }
 }
