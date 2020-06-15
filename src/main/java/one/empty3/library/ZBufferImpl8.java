fc055f8d373bb3f2977665d9d0b202bc43cdafab
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-06-15 14:39:28 +0200

    Update ZBufferImpl8.java

--- src/main/java/one/empty3/library/ZBufferImpl8.java
+++ src/main/java/one/empty3/library/ZBufferImpl8.java
@@ -156,7 +156,7 @@ class Data {
             int [] i = c;
             for(int y = 0; y<ha; y++) 
                for(int x= 0; x<la; x++) 
- i(f(container[y][x]!=null)
+ if((container[y][x]!=null)
    &&( container[y][x].texture()!=null)&&
   (  dataP[9][y][x]!=null)&&(dataP[10][y][x]!=null))
              
