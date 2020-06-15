77866841a034cd7ffb2d4aacff4ad66c5dd9b191
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-06-15 11:51:10 +0200

    Update ZBufferImpl8.java

--- src/main/java/one/empty3/library/ZBufferImpl8.java
+++ src/main/java/one/empty3/library/ZBufferImpl8.java
@@ -151,7 +151,7 @@ class Data {
             for(int y = 0; y<ha; y++) 
                for(int x= 0; x<la; x++) 
  if(container[y][x]!=null
-   && (container[y][x].texture()!=null)
+   && container[y][x].texture()!=null)
                    i[y + la * x] = container[y] [x]. texture().getColorAt(
                           dataP[9][y][x], dataP[10][y][x]);
              
