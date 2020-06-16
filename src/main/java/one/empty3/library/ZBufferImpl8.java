729a11afca5e54bde49b46e4f7a90145cd32d064
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-06-16 16:06:20 +0200

    Update ZBufferImpl8.java

--- src/main/java/one/empty3/library/ZBufferImpl8.java
+++ src/main/java/one/empty3/library/ZBufferImpl8.java
@@ -93,7 +93,7 @@ class Data {
         for(int j = 0; j<ha; j++) 
                for(int i= 0; i<la; i++) {
                    dataP[13][j][i] = 9999999999.9; //Double.MAX_VALUE;
-                   container[j][i] = INFINI;
+                   container[j][i] = ZBufferImpl8.INFINI;
                    dataP[9][j][i] = 1.0*i/la;
                    dataP[10][j][i] = 1.0*j/ha;  
                 
