a492641b4a58e28490fb1adb4ee9853033e6d21e
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-06-25 06:48:08 +0200

    Update and rename P3.java to vec.java

--- src/main/java/one/empty3/library/vec.java
+++ src/main/java/one/empty3/library/vec.java
@@ -1,14 +1,19 @@
 package one.empty3.library;
-public class P3 {
+public class vec {
      public static DoubleArray da;
      private int arrayNo;
      private int start;
      private int stop;
      private boolean disposable;
      
-     public P3(double x, double y, double z) {
+     public vec(double x, double y, double z) {
           start = da.addDoubles(3);
           da.setDoubles(start, x, y, z);
      }
 
+     public vec(double x, double y, double z,
+         double t) {
+          start = da.addDoubles(3);
+          da.setDoubles(start, x, y, z, t);
+     }
 }
