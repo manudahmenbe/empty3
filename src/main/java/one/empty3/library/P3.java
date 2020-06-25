2c42753bbc52036fd81c4e0b3f7f7dd2341cb9e0
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-06-25 06:32:36 +0200

    Update P3.java

--- src/main/java/one/empty3/library/P3.java
+++ src/main/java/one/empty3/library/P3.java
@@ -1,3 +1,10 @@
-package one.empty3.library.P3;
+package one.empty3.library;
 public class P3 {
+     public static DoubleArray da;
+     private int arrayNo;
+     private int start;
+     private int stop;
+     private boolean disposable;
+
+
 }
