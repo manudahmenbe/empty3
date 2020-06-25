8fc0e065ce41c3ae5b29aec77b2e379c78bd6757
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-06-25 06:54:27 +0200

    Update vec.java

--- src/main/java/one/empty3/library/vec.java
+++ src/main/java/one/empty3/library/vec.java
@@ -3,17 +3,28 @@ public class vec {
      public static DoubleArray da;
      private int arrayNo;
      private int start;
-     private int stop;
+     private int n;
      private boolean disposable;
-     
+     public int length() {
+         return length;
+     }
      public vec(double x, double y, double z) {
-          start = da.addDoubles(3);
+          n = 3;
+          start = da.addDoubles(n);
           da.setDoubles(start, x, y, z);
      }
 
      public vec(double x, double y, double z,
          double t) {
-          start = da.addDoubles(3);
+          n = 4;
+          start = da.addDoubles(n);
+         
           da.setDoubles(start, x, y, z, t);
+     }
+     public vec(vec v1, double... c) {
+     }
+     public vec(vec... v) {
+     }
+     public vec(double d, vec ... c) {
      }
 }
