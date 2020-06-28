cbffc93184d8cbd4ae834e31b33ab044b6de3b95
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-06-28 15:31:58 +0200

    Update TestThierryJaspart.java

--- src/test/java/tests2/TestThierryJaspart.java
+++ src/test/java/tests2/TestThierryJaspart.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 
 public class TestThierryJaspart extends TestObjetSub {
     private final Point3D[][] coeff = new Point3D[][]{
-            {Point3D.n(2., -2, 0), Point3D.n(2, -1, 0), Point3D.n(2, 0, 0), Point3D.n(2, 1, 0), Point3D.n(2, 2, 0)},
+            {Point3D.n(2, -2, 0), Point3D.n(2, -1, 0), Point3D.n(2, 0, 0), Point3D.n(2, 1, 0), Point3D.n(2, 2, 0)},
             {Point3D.n(1, -2, 0), Point3D.n(1, -1, 0), Point3D.n(1, 0, 0), Point3D.n(1, 1, 0), Point3D.n(1, 2, 0)},
             {Point3D.n(0, -2, 0), Point3D.n(0, -1, 0), Point3D.n(0, 0, 0), Point3D.n(0, 1, 0), Point3D.n(0, 2, 0)},
             {Point3D.n(-1, -2, 0), Point3D.n(-1, -1, 0), Point3D.n(-1, 0, 0), Point3D.n(-1, 1, 0), Point3D.n(-1, 2, 0)},
