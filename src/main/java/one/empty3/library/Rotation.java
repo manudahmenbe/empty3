4720c7d9872b1cbff16a4747875143e84a81b77a
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-06-28 19:36:10 +0200

    Update Rotation.java

--- src/main/java/one/empty3/library/Rotation.java
+++ src/main/java/one/empty3/library/Rotation.java
@@ -44,7 +44,7 @@ public class Rotation implements MatrixPropertiesObject
     protected boolean unmodified = true;
     public Rotation() {
             rot.setElem(new Matrix33(Matrix33.I));
-            centreRot.setElem(new Point3D(Point3D.O0.getCoordArr());
+            centreRot.setElem(new Point3D(Point3D.O0.getCoordArr()));
     }
     public Rotation(Matrix33 rot, Point3D centreRot) {
         this.rot.setElem(rot);
