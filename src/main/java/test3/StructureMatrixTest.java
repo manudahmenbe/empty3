1381184716c9c74f54b2d0e05f622016d22ba9f1
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-06-28 12:39:26 +0200

    Update StructureMatrixTest.java

--- src/main/java/test3/StructureMatrixTest.java
+++ src/main/java/test3/StructureMatrixTest.java
@@ -84,6 +84,7 @@ public class StructureMatrixTest {
     @Test
     public void addRow() throws Exception {
     }
+    /*
     @Test
     public void array2d() throws Exception
     {
@@ -106,7 +107,7 @@ public class StructureMatrixTest {
             }
 
     }
-
+*\
     @Test
     public void point3D()
     {
