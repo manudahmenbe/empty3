768fa56b269df2ae90f409040cfde7f05d799fe3
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-06-28 15:31:31 +0200

    Update StructureMatrixTest.java

--- src/test/java/test3/StructureMatrixTest.java
+++ src/test/java/test3/StructureMatrixTest.java
@@ -88,7 +88,7 @@ public class StructureMatrixTest {
     public void array2d() throws Exception
     {
         Point3D[][] coeff = new Point3D[][]{
-                {Point3D.n(2., -2, 0), Point3D.n(2, -1, 0), Point3D.n(2, 0, 0), Point3D.n(2, 1, 0), Point3D.n(2, 2, 0)},
+                {Point3D.n(2, -2, 0), Point3D.n(2, -1, 0), Point3D.n(2, 0, 0), Point3D.n(2, 1, 0), Point3D.n(2, 2, 0)},
                 {Point3D.n(1, -2, 0), Point3D.n(1, -1, 0), Point3D.n(1, 0, 0), Point3D.n(1, 1, 0), Point3D.n(1, 2, 0)},
                 {Point3D.n(0, -2, 0), Point3D.n(0, -1, 0), Point3D.n(0, 0, 0), Point3D.n(0, 1, 0), Point3D.n(0, 2, 0)},
                 {Point3D.n(-1, -2, 0), Point3D.n(-1, -1, 0), Point3D.n(-1, 0, 0), Point3D.n(-1, 1, 0), Point3D.n(-1, 2, 0)},
