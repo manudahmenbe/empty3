770f4f5db7540ce3235186c9bb718081ea7b12b1
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-06-30 10:13:06 +0200

    Update PointAlgebra.java

--- src/test/java/test3/PointAlgebra.java
+++ src/test/java/test3/PointAlgebra.java
@@ -1,5 +1,23 @@
 package test3;
+import one.empty3.library.*;
+import org.junit.Test;
 
+/*__
+ * Created by manue on 11-09-19.
+ */
 public class PointAlgebra {
+    @Test
+    public void testPoint3DInit() {
+      Double [] ds2 = new Double [] {1.4, 2.3. 3.1};
+      double [] ds1 = new Double [] {1.4, 2.3. 3.1};
+      assertTrue(new Point3D(ds1).equals(new Point3D(ds2)));
+      assertTrue(new Point3D(ds1).equals(new Point3D(1.4, 2.3, 3.1)));
+      assertFalse(new Point3D(ds1).equals(Point3D.O0));
+      assertTrue(new Point3D().equals(Point3D.O0));
+    }
 
+    @Test
+    public void testPoint3D() {
+        new Point3D();
+    }
 }
