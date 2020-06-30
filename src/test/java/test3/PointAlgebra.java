7f5b7bf4757a32f255894e2e5d61d99ff9a89efb
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-06-30 15:43:23 +0200

    Update PointAlgebra.java

--- src/test/java/test3/PointAlgebra.java
+++ src/test/java/test3/PointAlgebra.java
@@ -13,8 +13,9 @@ public class PointAlgebra {
       double [] ds1 = new double [] {1.4, 2.3, 3.1};
       //assertTrue(new Point3D(ds1).equals(new Point3D(ds2)));
       assertTrue(new Point3D(ds1).equals(new Point3D(1.4, 2.3, 3.1)));
-      //assertFalse(new Point3D(ds2).equals(Point3D.O0));
+      assertTrue(!new Point3D(ds1).equals(Point3D.O0));
       assertTrue(new Point3D().equals(Point3D.O0));
+        assertTrue(new Point3D(3).equals(Point3D.O0));
     }
 
     @Test
