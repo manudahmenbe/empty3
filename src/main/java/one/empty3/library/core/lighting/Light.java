ef439491e6885645e7f6b7fbd503495fff9fb45b
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-06-30 13:08:18 +0200

    Update Light.java

--- src/main/java/one/empty3/library/core/lighting/Light.java
+++ src/main/java/one/empty3/library/core/lighting/Light.java
@@ -27,9 +27,9 @@ public class Light {
               );//  (double)c.getAlpha());
 */
       
-          return new Point3D((double)cs[3],
+          return new Point3D((double)(cs[3]),
 
-              (double)cs[2],(double)cs[1] , (double)cs[0]);
+              (double)(cs[2]),(double)(cs[1]) , (double)(cs[0]));
       }
       public double level(double angle) {
            if(levels==null)
