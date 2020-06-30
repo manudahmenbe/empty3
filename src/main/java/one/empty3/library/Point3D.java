fefd92af3b53a760a51f0be2c8388414555a7a13
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-06-30 13:52:54 +0200

    Update Point3D.java

--- src/main/java/one/empty3/library/Point3D.java
+++ src/main/java/one/empty3/library/Point3D.java
@@ -94,38 +94,6 @@ public class Point3D extends Representable {
      }
 
 
-/*
- *  This file is part of Empty3.
- *
- *     Empty3 is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     Empty3 is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with Empty3.  If not, see <https://www.gnu.org/licenses/>. 2
- */
-
-/*
- * This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <https://www.gnu.org/licenses/>
- */
-
    
     /*__
      * *
@@ -195,8 +163,8 @@ public class Point3D extends Representable {
      * Initialise à partir d'un tableau
      *
      * @param x0 coordonnées (>3)
-     *//*
-    public Point3D(Double... x0) {
+     */
+    public Point3D(Double ... x0) {
          this(x0.length);
         int i=0;
         for(Double d : x0) {
@@ -205,7 +173,7 @@ public class Point3D extends Representable {
         }
         
     }
-*/
+
     public Point3D(Double[] x0, ITexture t) {
         int i=0;
         for(Double d : x0) {
