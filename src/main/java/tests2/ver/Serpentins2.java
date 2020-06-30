7c94508dbaab3806ecbf71f57ec5f73d3301dd2d
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-06-30 13:35:10 +0200

    Update Serpentins2.java

--- src/main/java/tests2/ver/Serpentins2.java
+++ src/main/java/tests2/ver/Serpentins2.java
@@ -83,7 +83,7 @@ public class Serpentins2 extends TestObjetSub {
     }
 
     public Point3D rand(double limitMinus, double limitPlus) {
-        Double[] d = new Double[3];
+        double[] d = new double[3];
         for (int i = 0; i < 3; i++) {
             d[i] = (limitPlus - limitMinus) * Math.random() + limitMinus;
         }
