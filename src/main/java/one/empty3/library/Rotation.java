181ab47a9336e1b83fbcb20f9e4366e4483035db
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-07-01 22:50:18 +0200

    Update Rotation.java

--- src/main/java/one/empty3/library/Rotation.java
+++ src/main/java/one/empty3/library/Rotation.java
@@ -42,11 +42,12 @@ public class Rotation implements MatrixPropertiesObject
     public static final StructureMatrix<Matrix33> rot1 = new StructureMatrix<>(0, Matrix33.class);
     public static final StructureMatrix<Point3D> centreRot1 = new StructureMatrix<>(0, Point3D.class);
    
-    protected StructureMatrix<Matrix33> rot = rot1;
-    protected StructureMatrix<Point3D> centreRot = centreRot1;
+    protected StructureMatrix<Matrix33> rot;
+    protected StructureMatrix<Point3D> centreRot;
     protected boolean unmodified = true;
     public Rotation() {
-            
+            rot  = rot1;
+            centreRot = centreRot1
             
     }
     public Rotation(Matrix33 rot, Point3D centreRot) {
