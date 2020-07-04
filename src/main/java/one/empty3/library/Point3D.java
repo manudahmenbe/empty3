f9a58937dde807fcee8298a09e36803e71a6d462
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-07-04 08:23:41 +0200

    Update Point3D.java

--- src/main/java/one/empty3/library/Point3D.java
+++ src/main/java/one/empty3/library/Point3D.java
@@ -58,7 +58,7 @@ public class Point3D extends Representable {
      public Point3D(int n) {
           super();
           if(da==null)
-              da = new DoubleArray(10, 1000000);
+              da = new DoubleArray(10, 100000000);
           start = da.addDoubles(n);
           this.n = n;
      }
