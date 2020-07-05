fa7fa9fc47d9743b6efc4e623db665392cfd9e24
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-07-05 14:36:19 +0200

    Create RFactory.java

---
+++ src/main/java/one/empty3/library/RFactory.java
@@ -0,0 +1,10 @@
+package one.empty3.library;
+public class RFactory {
+    public static void loadConfig() {
+        
+    }
+    // get interfaces
+    
+    public static ZBuffer getZBuffer();
+    public static Point3D getPoint3D;
+}
