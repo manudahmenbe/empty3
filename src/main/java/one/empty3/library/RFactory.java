5b6c70b12a03d5e868339ab8f86816786cd86c96
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-07-05 16:38:38 +0200

    Update RFactory.java

--- src/main/java/one/empty3/library/RFactory.java
+++ src/main/java/one/empty3/library/RFactory.java
@@ -1,10 +1,14 @@
 package one.empty3.library;
 public class RFactory {
     public static void loadConfig() {
-        
+        Properties load;
     }
     // get interfaces
     
-    public static ZBuffer getZBuffer();
-    public static Point3D getPoint3D;
+    public static ZBuffer getZBuffer() {
+        return ZBufferImpl8.class;
+        }
+    public static Point3D getPoint3D() {
+        return Point3D.class;
+    }
 }
