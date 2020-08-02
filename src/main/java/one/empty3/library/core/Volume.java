3f96eea3161b8af0195822db64ee93dc80b6ae64
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-08-02 08:58:30 +0200

    Create Volume.java

---
+++ src/main/java/one/empty3/library/core/Volume.java
@@ -0,0 +1,10 @@
+package one.empty3.library.core.*;
+
+public class Volume {
+     /* Comment dessiner un volume?
+        Cas particulier dessiner un volume
+        en parcourant un autre volume donné
+        incrémenter passer tracer conditions
+     */
+
+}
