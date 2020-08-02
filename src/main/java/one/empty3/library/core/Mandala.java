eab0ae8eb1ec76e3c5300dd4f50569bdc7516ec0
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-08-02 07:35:02 +0200

    Update and rename Mandala to Mandala.java

---
+++ src/main/java/one/empty3/library/core/Mandala.java
@@ -0,0 +1,20 @@
+package one.empty3.library.core;
+/***
+* curve. rotation. straight, curved. turtle
+* motif repeat at angle, copy paste
+* 
+  
+  
+*/
+public class Mandala extends RepresentableConteneur{ // Texture?
+    private ParametricSurface p;
+    public Mandala(ParametricSurface plan) {
+        this.p = plan;
+    }
+
+    public void addCurveX0_1__Y0_1(
+             ParametricCurve pc, double radius1,
+             double radius2, double angleRepeatDegree) {
+        
+    }
+}
