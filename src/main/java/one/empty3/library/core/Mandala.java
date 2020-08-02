5d887f798b59372312ccc3fdb9c16ecfb6d850c8
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-08-02 08:03:11 +0200

    Update Mandala.java

--- src/main/java/one/empty3/library/core/Mandala.java
+++ src/main/java/one/empty3/library/core/Mandala.java
@@ -2,16 +2,18 @@ package one.empty3.library.core;
 /***
 * curve. rotation. straight, curved. turtle
 * motif repeat at angle, copy paste
-* 
+* ex from center flow rotate draw
+ * borders in patterns 
   
   
 */
-public class Mandala extends RepresentableConteneur{ // Texture?
+public class Mandala extends ITexture {
     private ParametricSurface p;
-    public Mandala(ParametricSurface plan) {
+    public Mandala(double resX, double resY) {
         this.p = plan;
+        this.resX = resX;
+        this.resY = resY;
     }
-
     public void addCurveX0_1__Y0_1(
              ParametricCurve pc, double radius1,
              double radius2, double angleRepeatDegree) {
