53fe4892de405e1e920b6f769eba52bafb73a844
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-11-13 17:31:48 +0100

    Update Vec.java

--- src/main/java/one/empty3/library/shader/Vec.java
+++ src/main/java/one/empty3/library/shader/Vec.java
@@ -16,7 +16,7 @@ for(int i = 0; i<3; i++) {
 }
     public Vec(Double... comps) {
         
-         for(Double d : comps){
+         for(double d : comps){
              vecVal.add(1 ,d);
              }
     } 
