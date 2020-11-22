8f5bb9eebaa3831de2b0d3fd03e422d72fe3242f
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-11-22 15:19:40 +0100

    Update VecAlTree.java

--- src/main/java/one/empty3/library/shader/VecAlTree.java
+++ src/main/java/one/empty3/library/shader/VecAlTree.java
@@ -6,7 +6,7 @@ public class VecAlTree extends Vec {
 private boolean invalidTree = true;
     public VecAlTree(String formula, int dim) {
         super(dim);
-        String formulas = formula.split(",");
+        String [] formulas = formula.split(",");
         
             
 this.formula = formula;
