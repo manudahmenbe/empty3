8a7f659f4ad997076976fa7aaa8c43b82b050b7d
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-01-23 20:38:43 +0100

    Create ParseCode.java

---
+++ src/main/java/one/empty3/library/ParseCode.java
@@ -0,0 +1,21 @@
+package one.empty3.library;
+
+public class ParseCode {
+    String code;
+    //Tree tree;
+    public ParseCode() {
+        //tree = new Tree();
+    }
+    public void removeComments() {
+    }
+    public void block() {
+    }
+    public void tree() {
+    }
+    public void reduce() {
+    }
+    public void map() {
+    }
+    public void result() {
+    }
+}
