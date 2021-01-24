85f0cb8b0293fc6c1008599f3acb34669c1dbb6c
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-01-24 10:48:55 +0100

    Update ParseCode.java

--- src/main/java/one/empty3/library/ParseCode.java
+++ src/main/java/one/empty3/library/ParseCode.java
@@ -10,8 +10,40 @@ public class ParseCode {
         tree = new Tree();
         // separer en tokens typés
     }
-    public void removeComments() {
-        
+    public void removeComments(String brut) {
+        StringBuilder sb = new StringBuilder();
+                // Caractère par caractère
+        int comm = 0;
+        // Si /* sauter jusqu'à */
+        // Si // sauter jusqu'à ¶
+        for(int i=0; i<brut.length; i++) {
+            if(comm==0 && brut.charAt(i)=='/'
+               && i<brut.length-1
+               && brut.charAt(i+1)=='*')
+                comm = 1;
+            if(comm==0 && brut.charAt(i)=='/'
+               && i<brut.length-1
+               && brut.charAt(i+1)=='/') {
+    
+                comm = 2;
+                while(i<brut.length) {
+                    if(brut.charAt (i)=='\n'){
+                        comm = 0;
+                        i++;
+                    }
+                }
+            }
+            if(comm==1 && brut.charAt(i)=='*'
+               && i<brut.length-1
+               && brut.charAt(i+1)=='/') {
+                comm = 0; i+=2;
+            }
+            if(comm==0)
+                sb.append(brut.charAt(i));
+            i++;
+        }
+}
+            
     }
     public void block() {
     }
