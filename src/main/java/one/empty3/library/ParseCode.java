69f6b6531c7a96aa99dfbce5de41dae295a18dc9
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-01-24 09:28:15 +0100

    Update ParseCode.java

--- src/main/java/one/empty3/library/ParseCode.java
+++ src/main/java/one/empty3/library/ParseCode.java
@@ -1,12 +1,17 @@
 package one.empty3.library;
 import one.empty3.library.lang.*;
 public class ParseCode {
+    String tokenType = new String [] {
+       ""
+    };
     String code;
     Tree tree;
     public ParseCode() {
         tree = new Tree();
+        // separer en tokens typés
     }
     public void removeComments() {
+        
     }
     public void block() {
     }
