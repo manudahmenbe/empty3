88389a7033bbed61ae0d6473320e2a5419f47a66
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-01-25 10:15:09 +0100

    Update ParseCode.java

--- src/main/java/one/empty3/library/lang/ParseCode.java
+++ src/main/java/one/empty3/library/lang/ParseCode.java
@@ -2,10 +2,12 @@ package one.empty3.library.lang;
 
 import java.util.*;
 public class ParseCode {
-    public enum TokenType {Name, Keyword, StringLiteral,
-       FloatLiteral, DoubleLiteral, CharLiteral, Comment, JavadocComment
+    public enum TokenType {Name, Keyword,  Comment, JavadocComment
        };
+    public enum Literal {StringLiteral,
+       FloatLiteral, DoubleLiteral, CharLiteral };
     public enum InstructionBlock { Unnamed, For, While, Do, Method };
+    public enum Declaration {Package, Imports, Classes, Interfaces MethodMember, VarMember, Variable, Param};
     String special = "!%*&()+\\|/[]{}<>:,;?'\"";
     String[] keywords = new String [] {
        ""
