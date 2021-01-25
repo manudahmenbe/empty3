565a94758cd72be570ad77ef24ab17a01f978e5d
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-01-25 15:20:02 +0100

    Update Node.java

--- src/main/java/one/empty3/library/lang/Node.java
+++ src/main/java/one/empty3/library/lang/Node.java
@@ -1,15 +1,27 @@
 package one.empty3.library.lang;
 
-public class Node {
-    public static final int declaration = 0;
-    public static final int instruction = 1;
-    public static final int assignement = 2:
+import java.util.*;
 
+
+public class Node {
+
+    Node parent;
+    List<Node> children = new ArrayList();
+
+    public static final int declaration = 1;
+    public static final int instruction = 2;
+    public static final int assignement = 4:
+    TokenType tt;
     public enum TokenType {Name, Keyword,  Comment, JavadocComment
-       };
+        Literal};
     public enum Literal {StringLiteral,
-       FloatLiteral, DoubleLiteral, CharLiteral };
+        FloatLiteral, DoubleLiteral, CharLiteral };
     public enum InstructionBlock { Unnamed, For, While, Do, Method };
     public enum Declaration {Package, Imports, Classes, Interfaces, MethodMember, VarMember, Variable, Param};
     
+    public Node(TokenType tt, Literal l, 
+            String InstructionBlock ib,
+            Declaration d) {
+        this.tt = tt;
+    }
 }
