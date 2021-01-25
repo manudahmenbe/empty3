20851af88c7638a756796cd2c6209314180aea80
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-01-25 13:28:14 +0100

    Update Scope.java

--- src/main/java/one/empty3/library/lang/Scope.java
+++ src/main/java/one/empty3/library/lang/Scope.java
@@ -3,4 +3,16 @@ package one.empty3.library.lang;
 public class Scope {
     Scope parentScope;
     List<Node> nodes;
+    String modifier; // static
+    String privacy; // private public protected package
+    boolean iDo;
+    boolean iWhile;
+    boolean iUnnamed;
+    boolean iFor;
+    boolean iForEach;
+
+    Tree params;
+    Tree cond;
+    Tree instructions;
+    
 }
