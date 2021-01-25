1d1c5dd2a3ab2f920fd421f2b5269b0513caa85e
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-01-25 10:52:38 +0100

    Update Tree.java

--- src/main/java/one/empty3/library/lang/Tree.java
+++ src/main/java/one/empty3/library/lang/Tree.java
@@ -1,6 +1,11 @@
 package one.empty3.library.lang;
 
+import java.util.HasMap;
 public class Tree {
+    private HashMap<Scope, Tree>
     public Tree() {
+    }
+
+    public boolean add(Token token) {
     }
 }
