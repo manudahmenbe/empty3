c647820e8099c12561a07d7c861c01251ad4f90f
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-01-25 11:09:51 +0100

    Create Node.java

---
+++ src/main/java/one/empty3/library/lang/Node.java
@@ -0,0 +1,9 @@
+package one.empty3.library.lang;
+
+public class Node {
+    public static final int declaration = 0;
+    public static final int instruction = 1;
+    public static final int assignement = 2:
+
+    
+}
