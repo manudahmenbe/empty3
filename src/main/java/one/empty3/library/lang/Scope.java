75ad22284db1523fdff82b286216edd9f50af5dc
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-01-25 13:29:45 +0100

    Update Scope.java

--- src/main/java/one/empty3/library/lang/Scope.java
+++ src/main/java/one/empty3/library/lang/Scope.java
@@ -1,6 +1,17 @@
 package one.empty3.library.lang;
 
 public class Scope {
+public static final int declaration = 0;
+    public static final int instruction = 1;
+    public static final int assignement = 2:
+
+    public enum TokenType {Name, Keyword,  Comment, JavadocComment
+       };
+    public enum Literal {StringLiteral,
+       FloatLiteral, DoubleLiteral, CharLiteral };
+    public enum InstructionBlock { Unnamed, For, While, Do, Method };
+    public enum Declaration {Package, Imports, Classes, Interfaces, MethodMember, VarMember, Variable, Param};
+    
     Scope parentScope;
     List<Node> nodes;
     String modifier; // static
