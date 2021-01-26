a63ae83252766960729986476180beea6f434403
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-01-26 15:41:04 +0100

    Update Tree.java

--- src/main/java/one/empty3/library/lang/Tree.java
+++ src/main/java/one/empty3/library/lang/Tree.java
@@ -2,7 +2,10 @@ package one.empty3.library.lang;
 
 import java.util.HashMap;
 public class Tree {
+    String name;
+    String text;
     HashMap<Scope, Tree> trees;
+    Tree parent;
     public Tree() {
     }
 
