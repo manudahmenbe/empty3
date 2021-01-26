1a9366cb876ad1b6d0c168ce2456a47f88d9688e
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-01-26 16:54:25 +0100

    Update AlgebricTree.java

--- src/main/java/one/empty3/library/core/raytracer/tree/AlgebricTree.java
+++ src/main/java/one/empty3/library/core/raytracer/tree/AlgebricTree.java
@@ -50,7 +50,7 @@ public class AlgebricTree extends Tree {
     Map<String, Double> parametersValues = new HashMap<>();
     private Tree t;
     private TreeNode root;
-    private List<Classes.Objet> classes;
+    private List<Objet> classes;
     public AlgebricTree(String formula) throws AlgebraicFormulaSyntaxException {
         this.formula = formula;
     }
