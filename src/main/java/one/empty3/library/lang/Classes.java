b50e8e7e0f4afeae24ef4b23614651d661467802
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-01-26 13:37:19 +0100

    Update Classes.java

--- src/main/java/one/empty3/library/lang/Classes.java
+++ src/main/java/one/empty3/library/lang/Classes.java
@@ -1,9 +1,9 @@
 package one.empty3.library.lang;
 public class Classes {
-  private static classes;
-  private class Object {
-      private Class clazz;
-      private Object instance;
+  private static Objet classes;
+  class Objet {
+      Class clazz;
+      Object instance;
       
   }
 }
