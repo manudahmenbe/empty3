d6b12df42fdd5a8d9253bf8118a6f1d5db5ccca3
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-01-26 13:05:28 +0100

    Update and rename Clazz.java to Classes.java

---
+++ src/main/java/one/empty3/library/lang/Classes.java
@@ -0,0 +1,9 @@
+package one.empty3.library.lang;
+public class Classes {
+  private static classes;
+  private class Object {
+      private Class clazz;
+      private Object instance;
+      
+  }
+}
