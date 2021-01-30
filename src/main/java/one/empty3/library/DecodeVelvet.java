b2fc3d54ecee641581d1b314e51614c91724307c
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-01-30 11:31:08 +0100

    Update DecodeVelvet.java

--- src/main/java/one/empty3/library/DecodeVelvet.java
+++ src/main/java/one/empty3/library/DecodeVelvet.java
@@ -5,6 +5,17 @@ import java.util.ArrayList;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import javax.sound.sampled.AudioFormat;
+
+import com.zakgof.velvetvideo.IAudioDecoderStream;
+import com.zakgof.velvetvideo.IAudioEncoderStream;
+import com.zakgof.velvetvideo.IAudioFrame;
+import com.zakgof.velvetvideo.IDecodedPacket;
+import com.zakgof.velvetvideo.IDemuxer;
+import com.zakgof.velvetvideo.IMuxer;
+import com.zakgof.velvetvideo.IVelvetVideoLib;
+import com.zakgof.velvetvideo.MediaType;
+import com.zakgof.velvetvideo.impl.VelvetVideoLib;
 
 public class DecodeVelvet extends VideoDecoder {
    
