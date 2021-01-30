d6fe5cb3dc573dddf9012c81ddb3c062b72048d6
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-01-30 11:23:32 +0100

    Update DecodeVelvet.java

--- src/main/java/one/empty3/library/DecodeVelvet.java
+++ src/main/java/one/empty3/library/DecodeVelvet.java
@@ -1,5 +1,23 @@
-IVelvetVideoLib lib = VelvetVideoLib().getInstance();
-	try (IDemuxer demuxer = lib.demuxer(new File("/some/path/example.mp4"))) {
+package one.empty3.library;
+
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.io.File;
+
+public class DecodeVelvet extends VideoDecoder {
+   
+/***
+* init, start, run, and block on maxsize reached
+* @param file video to draw on surface
+* @param refTextureMov texture to apply
+*/
+    public VideoDecoder(File file, TextureMov refTextureMov) {
+        super(file, refTextureMov);
+
+   }
+   public void init() {
+	IVelvetVideoLib lib = VelvetVideoLib().getInstance();
+	try (IDemuxer demuxer = lib.demuxer(new File(file))) {
 	    IDecoderVideoStream videoStream = demuxer.videoStream(0);
 	    IFrame videoFrame;
 	    while ((videoFrame = videoStream.nextFrame()) != null) {
@@ -7,3 +25,5 @@ IVelvetVideoLib lib = VelvetVideoLib().getInstance();
 	   	    // Use image as needed...
 	    }
 	}      
+    }
+}
