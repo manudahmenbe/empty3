a0835db36e96048999300b002b977f5e3b059506
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-02-01 09:25:59 +0100

    Update DecodeOpenimaj.java

--- src/main/java/one/empty3/library/DecodeOpenimaj.java
+++ src/main/java/one/empty3/library/DecodeOpenimaj.java
@@ -4,5 +4,17 @@ public class DecodeOpenimaj {
 
 /*
 Video<MBFImage> video;
+video = new XuggleVideo(new File("/path/to/keyboardcat.flv"));
+VideoDisplay<MBFImage> display = VideoDisplay.createVideoDisplay(video)
+//VideoDisplay<MBFImage> display = VideoDisplay.createVideoDisplay(video);
+display.addVideoListener(
+  new VideoDisplayListener<MBFImage>() {
+    public void beforeUpdate(MBFImage frame) {
+        frame.processInplace(new CannyEdgeDetector());
+    }
+
+    public void afterUpdate(VideoDisplay<MBFImage> display) {
+    }
+  });
 */
 }
