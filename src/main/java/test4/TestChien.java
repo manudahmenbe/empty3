bb90e2e774cd0ba50a6b19f0d639351857b66e7e
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-02-12 10:00:01 +0100

    Update TestChien.java

--- src/main/java/test4/TestChien.java
+++ src/main/java/test4/TestChien.java
@@ -14,7 +14,7 @@ Crée des formes de base!
 Point3D tete = new Point3D(0.,0.,0. ); //tête 
 Point3D queue = new Point3D(1.,0.,1.); // queue
 Sphere tetes = new Sphere(tete, 0.4); //sphère 
-new Paralellipiped(tete,
+new Parallelepiped(tete,
    tete.plus(new Point3D(0,0,0.5)), 
    tete.plus(new Point3D( 1,0,0,0.5)),
    tete.plus(new Point3D(0,0,0.5 )));//parallel polyèdres largeur y 0.5
