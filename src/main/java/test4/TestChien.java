928cd572fa6c975df26798da23e1b31ecdb01125
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-02-12 13:28:05 +0100

    Update TestChien.java

--- src/main/java/test4/TestChien.java
+++ src/main/java/test4/TestChien.java
@@ -14,22 +14,22 @@ public class TestChien extends TestObjet{
        Point3D tete = new Point3D(0.,0.,0. ); //tête 
        Point3D queue = new Point3D(1.,0.,1.); // queue
        Sphere tetes = new Sphere(tete, 0.4); //sphère 
-       Parallelipiped corps = new Parallelepiped(tete,
-   new Point3D(0,0,0.5), 
-   new Point3D( 1,0,0,0.5),
-   new Point3D(0,0,0.5 ));//parallel polyèdres largeur y 0.5
+       Parallelepiped corps = new Parallelepiped(tete,
+   new Point3D(0.,0.,0.5), 
+   new Point3D( 1,0,0.,0.5),
+   new Point3D(0.,0.,0.5 ));//parallel polyèdres largeur y 0.5
    for(int i=0;i<4; i++) {
        patte[i] = new Tubulaire3();
        }
 
-       patte[0].add(P.n(0,0.25,0));
-       patte[0].add(P.n(0,0.25,1)); //patte avant 
-patte[1].add(P.n(0,-0.25,0));
-patte[1].add(P.n(0,-0.25,1));// patte avant #2
+       patte[0].add(P.n(0.,0.25,0.));
+       patte[0].add(P.n(0.,0.25,1.)); //patte avant 
+patte[1].add(P.n(0.,-0.25,0.));
+patte[1].add(P.n(0.,-0.25,1.));// patte avant #2
 patte[2].add(P.n(1,0.25,0));
 patte[2].add(P.n(1,0.25,1)); //patte arrière #1 
-patte[3].add(P.n(1,-0.25,0));
-patte[3].add(P.n(1,-0.25,1));// patte avant #2
+patte[3].add(P.n(1.,-0.25,0.));
+patte[3].add(P.n(1.,-0.25,1.));// patte avant #2
 //1,0,0 //etx queue.
     
 scene().add(corps);
