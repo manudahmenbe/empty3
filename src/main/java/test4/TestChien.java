4f0a645b797b7a94d252a1703fc8e9e83e561759
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-02-12 15:11:18 +0100

    Update TestChien.java

--- src/main/java/test4/TestChien.java
+++ src/main/java/test4/TestChien.java
@@ -10,26 +10,25 @@ public class TestChien extends TestObjetSub {
        Tubulaire3 [] patte = new Tubulaire3[4];
       // ts[0] =
 //Artifact : empty3-library-3d-gui ...
-//Crée des formes de base!
        Point3D tete = new Point3D(0.,0.,0. ); //tête 
        Point3D queue = new Point3D(1.,0.,1.); // queue
        Sphere tetes = new Sphere(tete, 0.4); //sphère 
-       Parallelepiped corps = new Parallelepiped(tete,
+       Parallelepiped corps = new Parallelepiped(new Point3D[]{tete,
    new Point3D(0.,0.,0.5), 
    new Point3D( 1.,0,0.,0.5),
-   new Point3D(0.,0.,0.5 ));//parallel polyèdres largeur y 0.5
+   new Point3D(0.,0.,0.5 )}, new TextuteCol(Color.RED);//parallel polyèdres largeur y 0.5
    for(int i=0;i<4; i++) {
        patte[i] = new Tubulaire3();
        }
 
-       patte[0].getSoulCurve().getElem(). getCoefficients().getElem().add(new Point3D(0.,0.25,0.));
+       patte[0].getSoulCurve().getElem(). getCoefficients().add(new Point3D(0.,0.25,0.));
        patte[0].getSoulCurve().getElem().getCoefficients().add(new Point3D(0.,0.25,1.)); //patte avant 
-patte[1].getSoulCurve().getElem().getCoefficients().getElem().add(new Point3D(0.,-0.25,0.));
-patte[1].getSoulCurve().getElem().getCoefficients().getElem().add(new Point3D(0.,-0.25,1.));// patte avant #2
-patte[2].getSoulCurve().getElem().getCoefficients().getElem().add(new Point3D(1,0.25,0));
-patte[2].getSoulCurve().getElem().getCoefficients().getElem().add(new Point3D(1,0.25,1)); //patte arrière #1 
-patte[3].getSoulCurve().getElem().getCoefficients().getElem().add(new Point3D(1.,-0.25,0.));
-patte[3].getSoulCurve().getElem().getCoefficients().getElem().add(new Point3D(1.,-0.25,1.));// patte avant #2
+patte[1].getSoulCurve().getElem().getCoefficients().add(new Point3D(0.,-0.25,0.));
+patte[1].getSoulCurve().getElem().getCoefficients().add(new Point3D(0.,-0.25,1.));// patte avant #2
+patte[2].getSoulCurve().getElem().getCoefficients().add(new Point3D(1,0.25,0));
+patte[2].getSoulCurve().getElem().getCoefficients().add(new Point3D(1,0.25,1)); //patte arrière #1 
+patte[3].getSoulCurve().getElem().getCoefficients().add(new Point3D(1.,-0.25,0.));
+patte[3].getSoulCurve().getElem().getCoefficients().add(new Point3D(1.,-0.25,1.));// patte avant #2
 //1,0,0 //etx queue.
     
 scene().add(corps);
