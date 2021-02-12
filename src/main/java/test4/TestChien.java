0d75988ee11182c5610ebcaf636b77ce1361c6e4
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-02-12 10:02:38 +0100

    Update TestChien.java

--- src/main/java/test4/TestChien.java
+++ src/main/java/test4/TestChien.java
@@ -7,17 +7,19 @@ import one.empty3.library.core.testing.*;
 public class TestChien {
 /*
     public void ginit() {
-       Tubulaire3 ts = new Tubulaire3[4];
+       Tubulaire3 [] patte = new Tubulaire3[4];
        ts[0] =
 Artifact : empty3-library-3d-gui ...
 Crée des formes de base!
 Point3D tete = new Point3D(0.,0.,0. ); //tête 
 Point3D queue = new Point3D(1.,0.,1.); // queue
 Sphere tetes = new Sphere(tete, 0.4); //sphère 
-new Parallelepiped(tete,
+Parallelipiped corps = new Parallelepiped(tete,
    tete.plus(new Point3D(0,0,0.5)), 
    tete.plus(new Point3D( 1,0,0,0.5)),
    tete.plus(new Point3D(0,0,0.5 )));//parallel polyèdres largeur y 0.5
+patte[0] = new Tubulaire();
+
 0,0.25,0 0,0.25,1 //patte avant 
 0,-0.25,1 0,-0.25,1// patte avant #2
 1,0.25,0 0,0.25,1 //patte arrière #1 
