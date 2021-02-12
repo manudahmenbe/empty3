7395599ab1abb42104ce7c18e88f007924e32b12
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-02-12 09:07:12 +0100

    Update TestChien.java

--- src/main/java/test4/TestChien.java
+++ src/main/java/test4/TestChien.java
@@ -7,17 +7,26 @@ public class TestChien {
        ts[0] =
 Artifact : empty3-library-3d-gui ...
 Crée des formes de base!
-0,0,0 tête 
-1,0,1 queue
-0,0,0 sphère 0,0,0.5 1,0,0,0.5 0,0,0.5 parallel polyèdres largeur y 0.5
-0,0.25,0 0,0.25,1 patte avant 0,-0.25,1 0,-0.25,1 patte avant #2
-1,0.25,0 0,0.25,1 patte arrière #1 1,-0.25,1 0,-0.25,1
-1,0,0 etx queue.
+0,0,0 //tête 
+1,0,1 // queue
+0,0,0 //sphère 
+0,0,0.5 1,0,0,0.5 0,0,0.5 //parallel polyèdres largeur y 0.5
+0,0.25,0 0,0.25,1 //patte avant 
+0,-0.25,1 0,-0.25,1// patte avant #2
+1,0.25,0 0,0.25,1 //patte arrière #1 
+1,-0.25,1 0,-0.25,1
+1,0,0 //etx queue.
 
 
 
 
 
 }
+
+Point3D ps = new Point3D[] {
+    P.n(0.,0.,0.),
+    P.n(1.,0.,1.),
+
+};
 */
 }
