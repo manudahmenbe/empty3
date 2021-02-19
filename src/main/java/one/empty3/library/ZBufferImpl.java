292f90b97a4ced2499b065a6a5f04d193779fe41
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-02-19 07:20:37 +0100

    Update ZBufferImpl.java

--- src/main/java/one/empty3/library/ZBufferImpl.java
+++ src/main/java/one/empty3/library/ZBufferImpl.java
@@ -140,7 +140,7 @@ public void copyResourceFiles(File destDirectory) {
     }
 
     public synchronized void draw() {
-        //camera(scene().cameraActive());
+        scene().cameraActive().calculerMatrice();
         scene().lumieres().clear();
         for (int i = 0; i < scene().getObjets().data1d.size(); i++)
             if (scene().getObjets().getElem(i).getClass().isAssignableFrom(Lumiere.class))
