61b4875511187d815205a60ad2545923c1a9a835
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-02-20 09:59:31 +0100

    Update Trajectoires.java

--- src/main/java/one/empty3/library/core/move/Trajectoires.java
+++ src/main/java/one/empty3/library/core/move/Trajectoires.java
@@ -41,9 +41,9 @@ public class Trajectoires {
     @Deprecated
     public static Point3D sphere(double longpc, double latpc, double radius) {
         return new Point3D(
-                Math.cos(longpc * Math.PI) * Math.cos(Math.PI * (latpc - 0.5)),
-                Math.sin(longpc * Math.PI) * Math.cos(Math.PI * (latpc - 0.5)),
-                Math.sin(Math.PI * (latpc - 0.5))
+                Math.cos(longpc * Math.PI) * Math.cos(Math.PI * (latpc)),
+                Math.sin(longpc * Math.PI) * Math.cos(Math.PI * (latpc)),
+                Math.sin(Math.PI * (latpc))
         ).mult(radius);
 
     }
