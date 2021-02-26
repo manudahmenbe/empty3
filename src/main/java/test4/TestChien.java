d7dba619ba77e8a23dfa6a1f523a283834c3c0f9
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-02-26 09:28:45 +0100

    Update TestChien.java

--- src/main/java/test4/TestChien.java
+++ src/main/java/test4/TestChien.java
@@ -11,7 +11,7 @@ public class TestChien extends TestObjetSub {
 
     public void ginit() {
         setMaxFrames(180);
-        //z.setDisplayType(ZBufferImpl.SURFACE_DISPLAY_COL_TRI);
+        z.setDisplayType(ZBufferImpl.SURFACE_DISPLAY_COL_TRI);
         scene().cameraActive().eye().changeTo(new Point3D(16., 0., 0.));
        Tubulaire3 [] patte = new Tubulaire3[4];
       // ts[0] =
