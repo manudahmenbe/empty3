27827602a72ae9bfa7e315db1d44dcee015fb89c
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-03-08 13:31:59 +0100

    Update TestMethodesCouleur.java

--- src/test/java/test3/TestMethodesCouleur.java
+++ src/test/java/test3/TestMethodesCouleur.java
@@ -7,6 +7,6 @@ import java awt.Color;
 public class TestMethodesCouleur extends TestCase {
     @Test
     public void testColorIdent() {
-        new Color(Lumiere.getInt(Lumiere.getRgb(Color.WHITE)));
+        assertTrue(new Color(Lumiere.getInt(Lumiere.getRgb(Color.WHITE))).equals(Color.WHITE));
     }
 }
