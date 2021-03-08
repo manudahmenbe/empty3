b11262c0c65802fd7d1eb60b3691ea858f8f4943
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-03-08 14:28:43 +0100

    Update TestMethodesCouleur.java

--- src/test/java/test3/TestMethodesCouleur.java
+++ src/test/java/test3/TestMethodesCouleur.java
@@ -2,7 +2,7 @@ package test3;
 
 import one.empty3.library.*;
 import org.junit.Test;
-import java awt.Color;
+import java.awt.Color;
 
 public class TestMethodesCouleur extends TestCase {
     @Test
