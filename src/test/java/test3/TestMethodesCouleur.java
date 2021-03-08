d7609c015291e3e9ffd82d8b941cdf68766e1223
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-03-08 13:24:00 +0100

    Update TestMethodesCouleur.java

--- src/test/java/test3/TestMethodesCouleur.java
+++ src/test/java/test3/TestMethodesCouleur.java
@@ -1,5 +1,10 @@
 package test3;
-import org.junit.*;
-public class TestMethodesCouleur extends TestCase {
 
+import one.empty3.library.*;
+import org.junit.Test;
+public class TestMethodesCouleur extends TestCase {
+    @Test
+    public void testColorIdent() {
+        Lumiere.getRgb(Color.WHITE);
+    }
 }
