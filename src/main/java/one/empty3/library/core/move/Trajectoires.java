a54136544050874848287522f4a06f88a4614d9c
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-03-09 23:02:17 +0100

    Create Trajectoires.java

--- src/main/java/one/empty3/library/core/move/Trajectoires.java
+++ src/main/java/one/empty3/library/core/move/Trajectoires.java
@@ -51,9 +51,9 @@ public class Trajectoires {
                 Math.sin(longpc * Math.PI) * Math.cos(Math.PI * (latpc)),
                 Math.sin(Math.PI * (latpc))
         ).mult(radius);
-        Point3D x = axe.prodVect(oo00).norme1().prodVect(axe);
+        Point3D x =axe.prodVect(oo00).norme1().prodVect(axe).norme1();
         Matrix33 matrix = new Matrix33(new Point3D[]{
-          x, axe.prodVect(oo00).norme1(), axe.norme1()}
+          x, axe.prodVect(x), axe}
            
         );
 
