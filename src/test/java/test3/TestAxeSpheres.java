25467162fcaf8a2e036505e8796963f138795f3a
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-03-09 18:32:23 +0100

    Update TestAxeSpheres.java

--- src/test/java/test3/TestAxeSpheres.java
+++ src/test/java/test3/TestAxeSpheres.java
@@ -1,5 +1,5 @@
 package test3;
-import one.empty3.library.core.lighting.Colors;
+import one.empty3.library.core.move.Trajectoires;
 import one.empty3.library.*;
 import org.junit.Test;
 import java.awt.Color;
@@ -8,5 +8,10 @@ import static org.junit.Assert.assertTrue;
 public class TestAxeSpheres {
     @Test
     public void testSphereTrajectoire() {
+         double r;
+         assertTrue(Trajectoires.sphere(Point3D.O0,
+            Point3D.random().norme1(), r=Math.random()*100.,
+               0, 0,  
+            ).norme()==r));
     }
 }
