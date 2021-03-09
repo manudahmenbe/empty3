5aa3c80298cf0ccf2bc09bc898db0fefc57e5458
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-03-09 19:52:08 +0100

    Update TestAxeSpheres.java

--- src/test/java/test3/TestAxeSpheres.java
+++ src/test/java/test3/TestAxeSpheres.java
@@ -15,28 +15,28 @@ public class TestAxeSpheres {
             ).norme()==r);
   assertTrue(Trajectoires.sphere(Point3D.X,
             Point3D.Z, r,
-               0, 0
-            ).equals(P.n(r+1., 0., 0.));
+               0, 0.
+            ).equals(P.n(r+1., 0., 0.)));
   assertTrue(Trajectoires.sphere(Point3D.X,
             Point3D.Z, r,
-               0.5, 0
-            ).equals(P.n(-r+1., 0., 0.));
+               0.5, 0.
+            ).equals(P.n(-r+1., 0., 0.)));
   assertTrue(Trajectoires.sphere(Point3D.X,
             Point3D.Z, r,
-               1.0, 0
-            ).equals(P.n(r+1., 0., 0.));
+               1.0, 0.
+            ).equals(P.n(r+1., 0., 0.)));
   assertTrue(Trajectoires.sphere(Point3D.Y,
             Point3D.Z, r,
-               1.0, 0
-            ).equals(P.n(r, 1., 0.));
+               1.0, 0.
+            ).equals(P.n(r, 1., 0.)));
   assertTrue(Trajectoires.sphere(Point3D.Y,
             Point3D.Z, r,
-               0.25, 0
-            ).equals(P.n(0., 1.+r, 0.));
-    }
+               0.25, 0.
+            ).equals(P.n(0., 1.+r, 0.)));
+    
   assertTrue(Trajectoires.sphere(Point3D.Y,
             Point3D.Z, r,
                0.75, 0
-            ).equals(P.n(0., 1.-r, 0.));
+            ).equals(P.n(0., 1.-r, 0.)));
     }
 }
