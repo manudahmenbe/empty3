9979c90a186806a8ad02ca11f199a9185cf7dd4e
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-03-09 18:24:58 +0100

    Update TestAxeSpheres.java

--- src/test/java/test3/TestAxeSpheres.java
+++ src/test/java/test3/TestAxeSpheres.java
@@ -1,1 +1,12 @@
-
+package test3;
+import one.empty3.library.core.lighting.Colors;
+import one.empty3.library.*;
+import org.junit.Test;
+import java.awt.Color;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue; 
+public class TestAxeSpheres {
+    @Test
+    public void testSphereTrajectoire() {
+    }
+}
