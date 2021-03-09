73b6313e2550b8c3447471f39c282e8db700d640
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-03-09 20:01:56 +0100

    Update TestAxeSpheres.java

--- src/test/java/test3/TestAxeSpheres.java
+++ src/test/java/test3/TestAxeSpheres.java
@@ -8,9 +8,10 @@ import static org.junit.Assert.assertTrue;
 public class TestAxeSpheres {
     @Test
     public void testSphereTrajectoire() {
+        for(int i=0; i<10; i++) {
          double r;
          assertTrue(Trajectoires.sphere(Point3D.O0,
-            Point3D.random().norme1(), Math.random(), Math.random(), r=Math.random()*100.
+            Point3D.random(10.).norme1(), Math.random(), Math.random(), r=Math.random()*100.
                
             ).norme()==r);
   assertTrue(Trajectoires.sphere(Point3D.X,
@@ -38,5 +39,6 @@ public class TestAxeSpheres {
             Point3D.Z, r,
                0.75, 0
             ).equals(P.n(0., 1.-r, 0.)));
+        }
     }
 }
