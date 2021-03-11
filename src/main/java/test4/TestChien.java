f23382f5b52ddffdef3a5b7afbb55a61d6a26e45
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-03-11 09:17:25 +0100

    Update TestChien.java

--- src/main/java/test4/TestChien.java
+++ src/main/java/test4/TestChien.java
@@ -56,7 +56,7 @@ Tubulaire3 corp;
         } 
 //        scene().cameraActive().getEye().setZ(-10.)
 
-//        scene().lumieres().add(new LumierePonctuelle(new Point3D(0.,0.,2.), Color.BLUE/*.YELLOW*/)) ;
+         scene().lumieres().add(new LumierePonctuelle(new Point3D(0.,0.,2.), Color.BLUE/*.YELLOW*/)) ;
    }
     public void finit() {
         Point3D sphere = Trajectoires.sphere(/*Point3D.O0, Point3D.Z, Point3D.X,*/
