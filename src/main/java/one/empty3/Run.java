029c8ea1fe93fd7f47bc6129f8f426144752fb49
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-03-15 11:51:13 +0100

    Update Run.java

--- src/main/java/one/empty3/Run.java
+++ src/main/java/one/empty3/Run.java
@@ -1,10 +1,15 @@
 package one.empty3.Run;
 
+import java.util.Properties;
 public class Run {
     public static void main(String [] args) {
         List<String> args2 = new ArrayList<>();
-        String line = new String [] [] {properties(), properties.getValue()};
-        System.scanf("Input : "+line[0]+"\ndefault value : " +line[1]+"\n"););
-        TestRun.main(args)
+        Properties properties = new Properties(new File("runtestobjetsub.txt"));
+        for((key, value ) -> {
+            String line = new String [] [] {properties(), properties.getValue()};
+            System.scanf("Input : "+line[0]+"\ndefault value : " +line[1]+"\n", s);
+            args2.add(line[0]+"="+s);
+        });
+TestRun.main(args2.toArray());
     }
 }
