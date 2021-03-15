83d620ed49de03fb23d3f0ae4c6925a0bcc4fcd0
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-03-15 11:40:20 +0100

    Update Run.java

--- src/main/java/one/empty3/Run.java
+++ src/main/java/one/empty3/Run.java
@@ -3,6 +3,8 @@ package one.empty3.Run;
 public class Run {
     public static void main(String [] args) {
         List<String> args2 = new ArrayList<>();
+        String line = new String [] [] {properties(), properties.getValue()};
+        System.scanf("Input : "+line[0]+"\ndefault value : " +line[1]+"\n"););
         TestRun.main(args)
     }
 }
