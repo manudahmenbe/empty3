3555949b0181bb61acd54314f88a0a5a323a8ec7
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-03-15 13:50:57 +0100

    Create Run.java

--- src/main/java/one/empty3/Run.java
+++ src/main/java/one/empty3/Run.java
@@ -7,16 +7,17 @@ public class Run {
     public static void main(String [] args) {
         List<String> args2 = new ArrayList<>();
         Properties properties = new Properties();
-        properties.load(new File("runtestobjetsub.txt"));
+        properties.load(new FileInputStream("runtestobjetsub.txt"));
         properties.forEach((key, value ) -> {
-            String line = new String [] [] {key, value};
-            System.in.scanf("Input : "+line[0]+"\ndefault value : " +line[1]+"\n", s);
+            String [] line = new String [] [] {key, value};
+            String s;
+            s = System.in.scanf("Input : "+line[0]+"\ndefault value : " +line[1]+"\n");
             if(s.length()>0) {
                args2.add(line[0]+"="+s);
                properties.put(key, s);
             }
         });
-        properties.save();
-TestRun.main(args2.toArray());
+        properties.save(new FileOutputStream("runtestobjetsub.txt"));
+        TestRun.main(args2.toArray());
     }
 }
