379ff6b1f9b2c24a170c5836017d7f4c4d23b14b
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-03-15 12:00:55 +0100

    Update Run.java

--- src/main/java/one/empty3/Run.java
+++ src/main/java/one/empty3/Run.java
@@ -1,6 +1,8 @@
 package one.empty3.Run;
 
 import java.util.Properties;
+import java.io.*;
+
 public class Run {
     public static void main(String [] args) {
         List<String> args2 = new ArrayList<>();
@@ -8,8 +10,12 @@ public class Run {
         for((key, value ) -> {
             String line = new String [] [] {properties(), properties.getValue()};
             System.scanf("Input : "+line[0]+"\ndefault value : " +line[1]+"\n", s);
-            args2.add(line[0]+"="+s);
+            if(s.length()>0) {
+               args2.add(line[0]+"="+s);
+               properties.put(key, s);
+            }
         });
+        properties.save();
 TestRun.main(args2.toArray());
     }
 }
