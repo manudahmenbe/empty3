a79d0a25ed8be5f9c6a27a74f1a082a97acd1397
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-03-18 20:08:38 +0100

    Update Camera.java

--- src/main/java/one/empty3/library/Camera.java
+++ src/main/java/one/empty3/library/Camera.java
@@ -150,7 +150,7 @@ public class Camera extends CameraBox {
     }
 
     public Point3D calculerPointDansRepere(Point3D p) {
-        Point3D p2 = matrice.getElem().mult(p.moins(getEye()));
+        Point3D p2 = p.plus(matrice.getElem().mult(p.moins(getEye())));
         p2.texture(p.texture());
         return p2;
     }
