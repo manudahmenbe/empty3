2597cd0e11a63fe263396cc3fc9fccab95760b03
Manuel DAHMEN <manuel.dahmen@gmx.com>
2021-04-03 14:20:58 +0200

    Change ECBufferedImage to ImageContainer.java DecodeOpenimaj.java

--- src/main/java/one/empty3/library/DecodeOpenimaj.java
+++ src/main/java/one/empty3/library/DecodeOpenimaj.java
@@ -1,77 +1,84 @@
 package one.empty3.library;
+
+import org.openimaj.image.processing.resize.BicubicInterpolation;
 import org.openimaj.video.*;
+
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.io.File;
+
 import org.openimaj.image.*;
 import org.openimaj.video.xuggle.XuggleVideo;
 import org.openimaj.image.ImageUtilities;
+
 public class DecodeOpenimaj extends VideoDecoder {
     protected boolean eof = false;
     protected TextureMov text;
     protected File file;
     protected boolean stop = false;
     protected static final long MAXSIZE = 4;
-    protected ArrayList<ECBufferedImage> imgBuf = new ArrayList() ;
+    protected ArrayList<ECBufferedImage> imgBuf = new ArrayList();
 
 
-/***
-* init, start, run, and block on maxsize reached
-* @param file video to draw on surface
-* @param refTextureMov texture to apply
-*/
+    /***
+     * init, start, run, and block on maxsize reached
+     * @param file video to draw on surface
+     * @param refTextureMov texture to apply
+     */
     public DecodeOpenimaj(File file, TextureMov refTextureMov) {
         super(file, refTextureMov);
         this.file = file;
         this.text = refTextureMov;
         //start();
 
-   }
- public int size() {
-  return imgBuf.size();
-  }
- public boolean isClosed() {
-  return eof;
-  }
- public ECBufferedImage current() {
- 
- ECBufferedImage c = imgBuf.get(0);
-  imgBuf.remove(0);
-  return c;
- 
- }
-
-public void run ()
-{
-
-    Video<MBFImage> video;
-
-    video = new XuggleVideo(file);
-
-    //VideoDisplay<MBFImage> display = VideoDisplay.createVideoDisplay(video);
-   
-    
-    for(MBFImage i : video) {
-       imgBuf.add(new ECBufferedImage(
-           ImageUtilities.createBufferedImage(i)));
-           while(imgBuf.size()>4) {
-               try {
-                   Thread.sleep(50);
-               } catch(Exception ex) {
-               }
-           }
     }
 
-    //VideoDisplay<MBFImage> display = VideoDisplay.createVideoDisplay(video);
-
-    /*display.addVideoListener(
-  new VideoDisplayListener<MBFImage>() {
-    public void beforeUpdate(MBFImage frame) {
-        frame.processInplace(new CannyEdgeDetector());
+    public int size() {
+        return imgBuf.size();
     }
 
-    public void afterUpdate(VideoDisplay<MBFImage> display) {
+    public boolean isClosed() {
+        return eof;
     }
-  });*/
-}
+
+    public ECBufferedImage current() {
+
+        ECBufferedImage c = imgBuf.get(0);
+        imgBuf.remove(0);
+        return c;
+
+    }
+
+    public void run() {
+
+        Video<MBFImage> video;
+
+        video = new XuggleVideo(file);
+
+        VideoDisplay<MBFImage> display = VideoDisplay.createVideoDisplay(video);
+
+
+        for (MBFImage i : video) {
+            imgBuf.add(new ECBufferedImage(
+                    ImageUtilities.createBufferedImage(i)));
+            while (imgBuf.size() > 4) {
+                try {
+                    Thread.sleep(50);
+                } catch (Exception ex) {
+                }
+            }
+        }
+
+
+        display.addVideoListener(
+                new VideoDisplayListener<>() {
+                    public void beforeUpdate(MBFImage frame) {
+                        frame.processInplace(new BicubicInterpolation(1000, 1000, 10));
+                    }
+
+                    public void afterUpdate(VideoDisplay<MBFImage> display) {
+                        imgBuf.add(new ECBufferedImage(display.getScreen().getDisplayedImage()));
+                    }
+                });
+    }
 }
