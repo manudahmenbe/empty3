4586381db635d67b45301ad0f0a0a9605e3787a8
Manuel DAHMEN <manuel.dahmen@gmx.com>
2021-04-18 13:45:13 +0200

    Humain, Lumiere améliorée

--- src/main/java/test4/Humain.java
+++ src/main/java/test4/Humain.java
@@ -8,14 +8,29 @@ import java.awt.*;
 import java.util.function.Predicate;
 
 public class Humain extends RepresentableConteneur {
-    public Point3D rotate1(Tubulaire3 tubulaire3, int index0, int indexP, double angle, double fract) {
-        return null;
+    protected double t;
+    public void rotateZ(Tubulaire3 tubulaire3, int index0, int indexP, double angle, double fract) {
+        Point3D elem0 = tubulaire3.getSoulCurve().getElem().getCoefficients().getElem(index0);
+        int size = tubulaire3.getSoulCurve().getElem().getCoefficients().getData1d().size();
+        int signum = (int) Math.signum(indexP - index0);
+
+        Matrix33 matrix33 = Matrix33.rotationZ(angle*fract);
+
+        for(int i=index0+signum; i<size&&i>=0; i+=signum) {
+            Point3D elemI = tubulaire3.getSoulCurve().getElem().getCoefficients().getElem(i);
+            Point3D mult = matrix33.mult(elemI.moins(elem0)).plus(elem0);
+            tubulaire3.getSoulCurve().getElem().getCoefficients().getData1d().set(i, mult);
+            System.out.print("i:" + i+" p:" + mult);
+        }
     }
     public void tubeAddPoint(Tubulaire3 tube, Point3D p) {
         tube.getSoulCurve().getElem().getCoefficients().getData1d().add(p);
     }
 
     public Humain() {
+    }
+    public void init() {
+        getListRepresentable().clear();
         Tubulaire3[] patte = new Tubulaire3[4];
         Point3D tete = new Point3D(0., 21., 0.); //tête
         Point3D queue = new Point3D(1., 0., 1.); // queue
@@ -28,35 +43,36 @@ public class Humain extends RepresentableConteneur {
             patte[i] = new Tubulaire3();
             patte[i].getSoulCurve().getElem().getCoefficients().getData1d().clear();
             patte[i].texture(new TextureCol(Color.ORANGE));
-            ((FctXY) (patte[i].getDiameterFunction().getElem())).setFormulaX("0.6");
+            patte[i].getDiameterFunction().getElem().setFormulaX("0.6");
         }
         Tubulaire3 corps;
         corps = new Tubulaire3();
         corps.getSoulCurve().getElem().getCoefficients().getData1d().clear();
-        corps.getSoulCurve().getElem().getCoefficients().setElem(P.n(0., 1., 0.));
-        corps.getSoulCurve().getElem().getCoefficients().setElem(P.n(1., 1., 0.));
+        corps.getSoulCurve().getElem().getCoefficients().setElem(P.n(0., 1., 0.), 0);
+        corps.getSoulCurve().getElem().getCoefficients().setElem(P.n(1., 1., 0.), 1);
         corps.texture(new TextureCol(Color.ORANGE));
-        ((FctXY) (corps.getDiameterFunction().getElem())).setFormulaX("1.5");
+        corps.getDiameterFunction().getElem().setFormulaX("1.5");
 // JAMBE AVANT DROIT
 // §1
         for (int i = 0; i < 2; i++) {
-            tubeAddPoint(patte[0], new Point3D(-1., 0., 2. * (2 * i - 1)));
-            tubeAddPoint(patte[0], new Point3D(0., 0., 2. * (2 * i - 1)));
-            tubeAddPoint(patte[0], new Point3D(0., 5., 2. * (2 * i - 1)));
-            tubeAddPoint(patte[0], new Point3D(0., 10., 2. * (2 * i - 1)));
-            tubeAddPoint(patte[0], new Point3D(0., 11., 0.));
-            tubeAddPoint(patte[0], new Point3D(0., 15., 0.));
-            tubeAddPoint(patte[0], new Point3D(0., 20., 0.));
-            tubeAddPoint(patte[0], new Point3D(0., 21., 0.));
+            tubeAddPoint(patte[i], new Point3D(-1., 0., 2. * (2 * i - 1)));
+            tubeAddPoint(patte[i], new Point3D(0., 0., 2. * (2 * i - 1)));
+            tubeAddPoint(patte[i], new Point3D(0., 5., 2. * (2 * i - 1)));
+            tubeAddPoint(patte[i], new Point3D(0., 10., 2. * (2 * i - 1)));
+            tubeAddPoint(patte[i], new Point3D(0., 11., 0.));
+            tubeAddPoint(patte[i], new Point3D(0., 15., 0.));
+            tubeAddPoint(patte[i], new Point3D(0., 20., 0.));
+            tubeAddPoint(patte[i], new Point3D(0., 21., 0.));
+            rotateZ(patte[i], 5, 0, Math.signum(i - 0.5), t);
+
         }
         for (int i = 0; i < 2; i++) {
-            tubeAddPoint(patte[0], new Point3D(0., 20., 1. * (2 * i - 1)));
-            tubeAddPoint(patte[0], new Point3D(0., 20., 2. * (2 * i - 1)));
-            tubeAddPoint(patte[0], new Point3D(0., 15., 2. * (2 * i - 1)));
-            tubeAddPoint(patte[0], new Point3D(0., 10., 2. * (2 * i - 1)));
-            tubeAddPoint(patte[0], new Point3D(0., 9., 2. * (2 * i - 1)));
-        }
-        for (int i = 0; i < 2; i++) {
+            tubeAddPoint(patte[i+2], new Point3D(0., 20., 1. * (2 * i - 1)));
+            tubeAddPoint(patte[i+2], new Point3D(0., 20., 2. * (2 * i - 1)));
+            tubeAddPoint(patte[i+2], new Point3D(0., 15., 2. * (2 * i - 1)));
+            tubeAddPoint(patte[i+2], new Point3D(0., 10., 2. * (2 * i - 1)));
+            tubeAddPoint(patte[i+2], new Point3D(0., 9., 2. * (2 * i - 1)));
+            rotateZ(patte[i+2], 1, 4, Math.signum(i - 0.5), t);
         }
         add(corps);
         add(tetes);
@@ -67,4 +83,11 @@ public class Humain extends RepresentableConteneur {
         }
     }
 
+    public double getT() {
+        return t;
+    }
+
+    public void setT(double t) {
+        this.t = t;
+    }
 }
--- src/main/java/test4/TestHumain.java
+++ src/main/java/test4/TestHumain.java
@@ -12,74 +12,31 @@ public class TestHumain extends TestObjetSub {
     public void tubeAddPoint(Tubulaire3 tube, Point3D p) {
         tube.getSoulCurve().getElem().getCoefficients().getData1d().add(p);
     }
-        public void ginit() {
-            setMaxFrames(18);
-            z.setDisplayType(ZBufferImpl.DISPLAY_ALL);
-            Tubulaire3[] patte = new Tubulaire3[4];
-            Point3D tete = new Point3D(0., 21., 0.); //tête
-            Point3D queue = new Point3D(1., 0., 1.); // queue
-            Sphere tetes = new Sphere(tete, 2.); //sphère
-            tetes.texture(new TextureCol(Color.RED));
-            queue.texture(new TextureCol(Color.BLACK));
 
-            Cube ventre = new Cube(5.0, P.n(0., 15., 0));
-            for (int i = 0; i < 4; i++) {
-                patte[i] = new Tubulaire3();
-                patte[i].getSoulCurve().getElem().getCoefficients().getData1d().clear();
-                patte[i].texture(new TextureCol(Color.ORANGE));
-                ((FctXY) (patte[i].getDiameterFunction().getElem())).setFormulaX("0.6");
-            }
-            Tubulaire3 corps;
-            corps = new Tubulaire3();
-            corps.getSoulCurve().getElem().getCoefficients().getData1d().clear();
-            corps.getSoulCurve().getElem().getCoefficients().setElem(P.n(0.,1.,0.));
-            corps.getSoulCurve().getElem().getCoefficients().setElem(P.n(1.,1.,0.));
-            corps.texture(new TextureCol(Color.ORANGE));
-            ((FctXY) (corps.getDiameterFunction().getElem())).setFormulaX("1.5");
-// JAMBE AVANT DROIT
-// §1
-            for(int i=0; i<2; i++) {
-            tubeAddPoint(patte[0], new Point3D(-1., 0.,  2.*(2*i-1)));
-            tubeAddPoint(patte[0], new Point3D(0., 0.,  2.*(2*i-1)));
-            tubeAddPoint(patte[0], new Point3D(0., 5.,  2.*(2*i-1)));
-            tubeAddPoint(patte[0], new Point3D(0., 10.,  2.*(2*i-1)));
-            tubeAddPoint(patte[0], new Point3D(0., 11.,  0.));
-            tubeAddPoint(patte[0], new Point3D(0., 15.,  0.));
-            tubeAddPoint(patte[0], new Point3D(0., 20.,  0.));
-            tubeAddPoint(patte[0], new Point3D(0., 21.,  0.));
-            }
-            for(int i=0; i<2; i++) {
-                tubeAddPoint(patte[0], new Point3D(0., 20.,  1.*(2*i-1)));
-                tubeAddPoint(patte[0], new Point3D(0., 20.,  2.*(2*i-1)));
-                tubeAddPoint(patte[0], new Point3D(0., 15.,  2.*(2*i-1)));
-                tubeAddPoint(patte[0], new Point3D(0., 10.,  2.*(2*i-1)));
-                tubeAddPoint(patte[0], new Point3D(0., 9.,   2.*(2*i-1)));
-            }
-            for(int i=0; i<2; i++) {
-            }
-                scene().add(corps);
-            scene().add(tetes);
-            scene().add(ventre);
-            for (int i = 0; i < 4; i ++) {
-                scene().add(patte[i]);
-
-            }
-            scene().lumieres().add(new LumierePonctuelle(new Point3D(10., 10., 2.), Color.BLUE));
-        }
-
-        public void finit() {
-            scene().cameras().clear();
-            Camera c = new Camera(Point3D.Z.mult(-40.), Point3D.O0, Point3D.Y);
-            scene().cameras().add(c);
-            c.declareProperties();
-            scene().cameraActive(c);
-        }
-
-        public static void main(String [] args) {
-            TestHumain testHumain = new TestHumain();
-            testHumain.setPublish(true);
-            new Thread(testHumain).start();
-        }
-
+    public void ginit() {
+        setMaxFrames(18);
+        z.setDisplayType(ZBufferImpl.DISPLAY_ALL);
+        scene().lumieres().add(new LumierePonctuelle(new Point3D(10., 10., 2.), Color.BLUE));
     }
+
+    public void finit() {
+        scene().cameras().clear();
+        Camera c = new Camera(Point3D.Z.mult(-40.), Point3D.O0, Point3D.Y);
+        scene().cameras().add(c);
+        c.declareProperties();
+        scene().cameraActive(c);
+
+        Humain humain = new Humain();
+        scene().add(humain);
+        humain.setT(1.0*frame()/getMaxFrames());
+        humain.init();
+    }
+
+    public static void main(String[] args) {
+        TestHumain testHumain = new TestHumain();
+        testHumain.setPublish(true);
+        new Thread(testHumain).start();
+    }
+
+}
 
