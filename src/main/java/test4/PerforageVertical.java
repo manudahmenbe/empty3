a4239cbcb351b2c0b808e769ab18aa92c9a7dbe3
Manuel DAHMEN <manuel.dahmen@gmx.com>
2021-04-26 19:42:41 +0200

    ZBufferImpl.java Camera2Quad.java

--- src/main/java/test4/PerforageVertical.java
+++ src/main/java/test4/PerforageVertical.java
@@ -14,7 +14,7 @@ public class PerforageVertical extends TestObjetSub {
         Tubulaire3 tubulaire3 = new Tubulaire3();
         tubulaire3.getSoulCurve().getElem().getCoefficients().getData1d().add(new Point3D(Math.sqrt(2), 0., Math.sqrt(2)));
         tubulaire3.getSoulCurve().getElem().getCoefficients().getData1d().add(new Point3D(Math.sqrt(2), 0., Math.sqrt(2)).plus(Point3D.Y));
-        tubulaire3.getDiameterFunction().getElem().setFormulaX("" + Math.sqrt(2.));
+        tubulaire3.getDiameterFunction().getElem().setFormulaX("0.8");
         scene().add(tubulaire3);
         Tubulaire3 cs1 = new Tubulaire3();
         Tubulaire3 cs2 = new Tubulaire3();
@@ -26,11 +26,11 @@ public class PerforageVertical extends TestObjetSub {
         cs2.getSoulCurve().getElem().getCoefficients().getData1d().add(Point3D.Z.moins(Point3D.Y));
         scene().add(cs1);
         scene().add(cs2);
-        Sphere sphere1 = new Sphere(P.n(0., 1., 0.3), 0.3);
-        Sphere sphere2 = new Sphere(P.n(0.3, 1., 0.), 0.3);
+        Sphere sphere1 = new Sphere(P.n(0., 1.0, 0.3), 0.3);
+        Sphere sphere2 = new Sphere(P.n(0.3, 1.0, 0.0), 0.3);
         scene().add(sphere1);
         scene().add(sphere2);
-        Camera camera = new Camera(Point3D.Z.mult(-4.), Point3D.O0, Point3D.Z.plus(Point3D.X));
+        Camera camera = new Camera(Point3D.Z.mult(-4.), Point3D.O0, Point3D.Z/*.plus(Point3D.X)*/);
         scene().cameras().add(camera);
         camera.declareProperties();
         scene().cameraActive(camera);
@@ -39,7 +39,7 @@ public class PerforageVertical extends TestObjetSub {
 
     public static void main(String[] args) {
         PerforageVertical perforageVertical = new PerforageVertical();
-        perforageVertical.setMaxFrames(10000);
+        perforageVertical.setMaxFrames(100);
         perforageVertical.setPublish(true);
         perforageVertical.setGenerate(GENERATE_MODEL|GENERATE_IMAGE|GENERATE_MOVIE);
         Thread thread = new Thread(perforageVertical);
