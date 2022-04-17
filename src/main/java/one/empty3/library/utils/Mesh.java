10aa1ab0815f1809d59bdf3a1767c629c1cbc059
dathewolf <manuel.dahmen@gmx.com>
2022-04-17 21:15:25 +0200

    Polygons

--- src/main/java/one/empty3/library/utils/Mesh.java
+++ src/main/java/one/empty3/library/utils/Mesh.java
@@ -6,8 +6,7 @@ import one.empty3.library.StructureMatrix;
 import one.empty3.library.core.nurbs.ParametricSurface;
 
 public class Mesh {
-    public static Polygons createMesh(Point3D center, Point3D [] directions, int su, int sv) {
-        Polygons polygons = new Polygons();
+    public static StructureMatrix<Point3D> createMesh(Point3D center, Point3D [] directions, int su, int sv) {
         StructureMatrix<Point3D> point3DStructureMatrix = new StructureMatrix<>(2, Point3D.class);
         for(int i=0; i<su; i++) {
             for(int j=0; j<sv; j++) {
@@ -15,16 +14,16 @@ public class Mesh {
                         .plus(directions[1].mult(j))), i, j);
             }
         }
-        return polygons;
+        return point3DStructureMatrix;
     }
-    public static Polygons createMeshOnSurface(Point3D center, ParametricSurface ps, int su, int sv) {
-        Polygons polygons = new Polygons();
+    public static StructureMatrix<Point3D> createMeshOnSurface(Point3D center, ParametricSurface ps, int su, int sv) {
         StructureMatrix<Point3D> point3DStructureMatrix = new StructureMatrix<>(2, Point3D.class);
         for(int i=0; i<su; i++) {
             for(int j=0; j<sv; j++) {
                 point3DStructureMatrix.setElem(ps.calculerPoint3D(1.0*i/su, 1.0*j/sv), i, j);
             }
         }
-        return polygons;
+        return point3DStructureMatrix;
     }
+    //Cherche un modèle de mesh et éditeur et rendu.
 }
