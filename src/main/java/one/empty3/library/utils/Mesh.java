5faf79998120b4685802a404ee9c8e3b6804060f
dathewolf <manuel.dahmen@gmx.com>
2022-04-17 20:10:47 +0200

    Polygons

--- src/main/java/one/empty3/library/utils/Mesh.java
+++ src/main/java/one/empty3/library/utils/Mesh.java
@@ -1,10 +1,19 @@
 package one.empty3.library.utils;
 
 import one.empty3.library.Point3D;
+import one.empty3.library.Polygons;
 import one.empty3.library.StructureMatrix;
 
 public class Mesh {
-    public static StructureMatrix<Point3D> createMesh(Point3D center, Point3D directions, int[] sizes) {
-        StructureMatrix<Point3D> matrix = new StructureMatrix<>(sizes.length, Point3D.class);
+    public static Polygons createMesh(Point3D center, Point3D [] directions, int[] sizes) {
+        Polygons polygons = new Polygons();
+        StructureMatrix<Point3D> point3DStructureMatrix = new StructureMatrix<>(2, Point3D.class);
+        for(int i=0; i<sizes[0]; i++) {
+            for(int j=0; j<sizes[1]; j++) {
+                point3DStructureMatrix.setElem(center.plus(directions[0].mult(i)
+                        .plus(directions[1].mult(j))), i, j);
+            }
+        }
+        return polygons;
     }
 }
